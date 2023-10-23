6441a6af1b446386730d1023a125b7057e9b688e
임성원 <dnjsndnjs@gmail.com>
2023-10-23 16:28:54 +0900

    Feat: 뒤로가기 버튼 설정

--- android/app/src/main/java/com/finale/neulhaerang/MainActivity.kt
+++ android/app/src/main/java/com/finale/neulhaerang/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.finale.neulhaerang
 
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import com.finale.neulhaerang.ui.App
 
 /**
@@ -14,7 +23,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            BackOnPressed()
             App()
         }
+    }
+}
+
+/*
+ * 뒤로 가기 두 번 눌렀을 때 앱 종료
+ */
+@Composable
+fun BackOnPressed() {
+    val context = LocalContext.current
+    var backPressedState by remember { mutableStateOf(true) }
+    var backPressedTime = 0L
+
+    BackHandler(enabled = backPressedState) {
+        if (System.currentTimeMillis() - backPressedTime <= 1000L) {
+            // 앱 종료
+            (context as Activity).finish()
+        } else {
+            backPressedState = true
+            Toast.makeText(context, "한 번 더 누르시면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }
