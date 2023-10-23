a3d0571c33389e48b43a9286567b6364be38f9e3
jungeunevepark <evecomcom98@naver.com>
2023-10-23 15:43:39 +0900

    Init: 백엔드 프로젝트 생성

---
+++ server/src/main/java/com/finale/neulhaerang/NeulhaerangApplication.java
@@ -0,0 +1,13 @@
+package com.finale.neulhaerang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeulhaerangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeulhaerangApplication.class, args);
+	}
+
+}
