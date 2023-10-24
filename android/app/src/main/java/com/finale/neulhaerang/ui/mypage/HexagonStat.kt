5e513ba46b24eac307640e28fc7a391f3821a124
kjy6135@naver.com <kjy6135@naver.com>
2023-10-24 16:13:40 +0900

    Feat: 육각 능력치 UI 추가

---
+++ android/app/src/main/java/com/finale/neulhaerang/ui/mypage/HexagonStat.kt
@@ -0,0 +1,15 @@
+package com.finale.neulhaerang.ui.mypage
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ *  HexagonStat
+ *  육각 스탯 UI 입니다.
+ *  6가지의 능력치를 받아 육각 그래프 형태로 표시합니다.
+ */
+
+@Composable
+fun HexagonStat(){
+    Text("육각 스텟 UI 입니당")
+}
