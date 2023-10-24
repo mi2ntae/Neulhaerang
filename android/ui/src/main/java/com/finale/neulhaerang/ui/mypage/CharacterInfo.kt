47ca50b16c2484a737a5fa43a81a36f30293863a
kjy6135@naver.com <kjy6135@naver.com>
2023-10-24 16:12:37 +0900

    Feat: 캐릭터 정보 UI 추가

---
+++ android/ui/src/main/java/com/finale/neulhaerang/ui/mypage/CharacterInfo.kt
@@ -0,0 +1,14 @@
+package com.finale.neulhaerang.ui.mypage
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ *  CharacterInfo
+ *  캐릭터(표정, 가방, 스킨, 안경, 모자, 무기, 목도리)
+ *  정보를 보여주는 UI 입니다.
+ */
+@Composable
+fun CharacterInfo(){
+    Text("캐릭터 정보입니당")
+}
