748d82e383b9208ad7aa2bc187b191ffd47f0fcf
임성원 <dnjsndnjs@gmail.com>
2023-10-24 17:24:09 +0900

    Feat: 체크리스트 생성 페이지 TopBar 레이아웃

--- android/app/src/main/java/com/finale/neulhaerang/ui/checklistCreation/ChecklistCreationScreen.kt
+++ android/app/src/main/java/com/finale/neulhaerang/ui/checklistCreation/ChecklistCreationScreen.kt
@@ -3,37 +3,60 @@ package com.finale.neulhaerang.ui.checklistCreation
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.finale.neulhaerang.ui.theme.NeulHaeRangTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
-fun ChecklistCreationScreen() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopBar(Modifier.fillMaxWidth())
-        }
-    ) {
+fun ChecklistCreationScreen(navController: NavHostController) {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        TopAppBar(
+            title = { Text(text = "체크리스트 작성") },
+            navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "뒤로 가기")
+                }
+            },
+            actions = {
+                TextButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "취소", color = MaterialTheme.colorScheme.secondary)
+                }
+                TextButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "완료", color = MaterialTheme.colorScheme.primary)
+                }
+            },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            )
+        )
+    }) {
         Content(
             modifier = Modifier
-                .padding(it)
+                .padding(paddingValues = it)
                 .fillMaxSize()
         )
-    }
-}
-
-@Composable
-fun TopBar(modifier: Modifier = Modifier) {
-    Row(modifier = modifier) {
-        Text(text = "top bar")
     }
 }
 
@@ -72,5 +95,13 @@ fun TodoCreation(modifier: Modifier = Modifier) {
         Row {
             Text(text = "날짜")
         }
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    NeulHaeRangTheme {
+        ChecklistCreationScreen(navController = rememberNavController())
     }
 }
