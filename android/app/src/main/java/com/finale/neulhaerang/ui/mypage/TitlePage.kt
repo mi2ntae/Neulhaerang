ac74faf690398b7f5fe60150cc941079657e8ab1
kjy6135@naver.com <kjy6135@naver.com>
2023-10-24 16:13:55 +0900

    Feat: 칭호 페이지 UI 추가

---
+++ android/app/src/main/java/com/finale/neulhaerang/ui/mypage/TitlePage.kt
@@ -0,0 +1,14 @@
+package com.finale.neulhaerang.ui.mypage
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ *  TitlePage
+ *  칭호를 볼 수 있는 페이지입니다.
+ */
+
+@Composable
+fun TitlePage() {
+    Text("칭호입니당")
+}
