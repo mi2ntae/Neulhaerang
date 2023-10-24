0c942707b5d0e859377e6f6a81176c010238d794
임성원 <dnjsndnjs@gmail.com>
2023-10-24 18:23:25 +0900

    Feat: 체크리스트 생성 페이지 UI 루틴 스위치

--- android/app/src/main/java/com/finale/neulhaerang/ui/checklistCreation/ChecklistCreationScreen.kt
+++ android/app/src/main/java/com/finale/neulhaerang/ui/checklistCreation/ChecklistCreationScreen.kt
@@ -1,23 +1,36 @@
 package com.finale.neulhaerang.ui.checklistCreation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.finale.neulhaerang.ui.theme.NeulHaeRangTheme
@@ -62,12 +75,18 @@ fun ChecklistCreationScreen(navController: NavHostController) {
 
 @Composable
 fun Content(modifier: Modifier = Modifier) {
+    // 동작 확인용 변수
+    // TODO ViewModel 구현
+    var isRoutine by remember { mutableStateOf(false) }
+
     Column(modifier = modifier) {
         Row {
             Text(text = "체크리스트 이름")
         }
-        Row {
-            Text(text = "루틴 여부 스위치")
+        ChecklistCreationItem(name = "루틴", icon = Icons.Filled.Refresh) {
+            Switch(
+                checked = isRoutine,
+                onCheckedChange = { isRoutine = it })
         }
         RoutineCreation()
         TodoCreation()
@@ -77,6 +96,29 @@ fun Content(modifier: Modifier = Modifier) {
         Row {
             Text(text = "알림")
         }
+    }
+}
+
+@Composable
+fun ChecklistCreationItem(
+    modifier: Modifier = Modifier,
+    name: String,
+    icon: ImageVector,
+    content: @Composable () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 4.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(imageVector = icon, contentDescription = name)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = name)
+        }
+        content()
     }
 }
 
