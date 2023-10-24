9d59a5bb90078d4803d1e78b55cf3063f7e89712
kjy6135@naver.com <kjy6135@naver.com>
2023-10-24 16:13:27 +0900

    Feat: 마이페이지 셀렉션 바 UI 추가

---
+++ android/app/src/main/java/com/finale/neulhaerang/ui/mypage/MyPageSelectionBar.kt
@@ -0,0 +1,15 @@
+package com.finale.neulhaerang.ui.mypage
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ *  MyPageSelectionBar
+ *  마이페이지에서 능력치, 의상실, 칭호를 선택할 때
+ *  사용하는 Row 형태의 UI 입니다.
+ */
+
+@Composable
+fun MyPageSelectionBar() {
+    Text("옵션 선택 바 입니다.")
+}
