3fcaa7a1be241a359dc8a60f51540ee8ccd1a3bb
kosigi19 <kosigi19@naver.com>
2023-10-26 17:57:13 +0900

    Feat: 요일 Element UI 개발

---
+++ android/ui/src/main/java/com/finale/neulhaerang/ui/app/main/CheckListCalendar.kt
@@ -0,0 +1,66 @@
+package com.finale.neulhaerang.ui.app.main
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.finale.neulhaerang.ui.R
+
+/**
+ *  CheckListCalendar
+ *  루틴과 To do를 보여주는 캘린더
+ */
+@Composable
+fun DayItem() {
+    Column {
+        Text("MON")
+        Text("나태함")
+        Text("17")
+    }
+
+}
+
+@Composable
+fun DayElement(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Text(text = stringResource(R.string.monday))
+        Box(
+            modifier = modifier
+                .size(48.dp)
+                .clip(CircleShape)
+                .background(Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.soso),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .background(Color.Blue)
+            )
+        }
+        Text(text = "15")
+
+    }
+}
