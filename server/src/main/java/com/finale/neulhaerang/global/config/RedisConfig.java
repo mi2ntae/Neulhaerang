e7238df3958774f4d8b2a939fa31037db10af210
김민태 <kimmintae981019@gmail.com>
2023-10-26 17:35:26 +0900

    Feat: Redis Template 추가 및 예외 작성

--- server/src/main/java/com/finale/neulhaerang/global/config/RedisConfig.java
+++ server/src/main/java/com/finale/neulhaerang/global/config/RedisConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -28,4 +30,18 @@ public class RedisConfig {
 
 		return new LettuceConnectionFactory(redisConfiguration);
 	}
+
+	@Bean
+	public RedisTemplate<String, String> redisTemplate() {
+		// redisTemplate를 받아와서 set, get, delete를 사용
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+		// setKeySerializer, setValueSerializer 설정
+		// redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+		return redisTemplate;
+	}
+
 }
