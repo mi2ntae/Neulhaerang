12ca929b3d0a3089db28f85997ab8a2ccfe6c091
김민태 <kimmintae981019@gmail.com>
2023-10-27 14:50:15 +0900

    Feat: 엑세스 토큰 재발급 로직 추가

--- server/src/main/java/com/finale/neulhaerang/domain/member/service/AuthServiceImpl.java
+++ server/src/main/java/com/finale/neulhaerang/domain/member/service/AuthServiceImpl.java
@@ -1,17 +1,25 @@
 package com.finale.neulhaerang.domain.member.service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.finale.neulhaerang.domain.member.dto.request.LoginReqDto;
+import com.finale.neulhaerang.domain.member.dto.request.TokenReqDto;
 import com.finale.neulhaerang.domain.member.dto.response.KakaoUserResDto;
 import com.finale.neulhaerang.domain.member.dto.response.LoginResDto;
+import com.finale.neulhaerang.domain.member.dto.response.TokenResDto;
 import com.finale.neulhaerang.domain.member.entity.Device;
 import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.member.repository.DeviceRepository;
 import com.finale.neulhaerang.domain.member.repository.MemberRepository;
+import com.finale.neulhaerang.global.exception.common.ExpiredAuthException;
+import com.finale.neulhaerang.global.exception.common.NonValidJwtTokenException;
 import com.finale.neulhaerang.global.util.JwtTokenProvider;
+import com.finale.neulhaerang.global.util.RedisUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,9 +31,16 @@ public class AuthServiceImpl implements AuthService{
 	private final MemberRepository memberRepository;
 	private final DeviceRepository deviceRepository;
 	private final JwtTokenProvider jwtTokenProvider;
+	private final RedisUtil redisUtil;
 
 	// Feign Client
 	private final KakaoInfoFeignClient kakaoInfoFeignClient;
+
+	@Value("${spring.jwt.token.access-expiration-time}")
+	private long accessExpirationTime;
+
+	@Value("${spring.jwt.token.refresh-expiration-time}")
+	private long refreshExpirationTime;
 
 	@Override
 	public LoginResDto login(LoginReqDto loginReqDto) {
@@ -40,8 +55,29 @@ public class AuthServiceImpl implements AuthService{
 				break;
 		}
 		String accessToken = jwtTokenProvider.createAccessToken(loginReqDto.getDeviceToken(), member.getId());
-		jwtTokenProvider.createRefreshToken(loginReqDto.getDeviceToken(), member.getId());
-		return LoginResDto.of(member, accessToken);
+		String refreshToken = jwtTokenProvider.createRefreshToken(loginReqDto.getDeviceToken());
+		LocalDateTime expire_time = LocalDateTime.now().plus(accessExpirationTime, ChronoUnit.MILLIS);
+		log.info(expire_time.toString());
+		return LoginResDto.of(member, TokenResDto.of(accessToken, refreshToken, expire_time));
+	}
+
+	@Override
+	public TokenResDto refreshAccessToken(TokenReqDto tokenReqDto) throws NonValidJwtTokenException, ExpiredAuthException {
+		String deviceToken = jwtTokenProvider.getSubject(tokenReqDto.getAccessToken());
+		long memberId = Long.parseLong(String.valueOf(jwtTokenProvider.getClaims(tokenReqDto.getAccessToken()).get("member")));
+		String refreshToken = tokenReqDto.getRefreshToken();
+		String savedRefreshToken = redisUtil.getData(tokenReqDto.getRefreshToken());
+		if(savedRefreshToken != null)  {
+			refreshToken = jwtTokenProvider.createRefreshToken(deviceToken);
+			if(!savedRefreshToken.equals(refreshToken)) {
+				throw new NonValidJwtTokenException();
+			}
+			redisUtil.setData(deviceToken, refreshToken, refreshExpirationTime);
+		} else {
+			throw new ExpiredAuthException();
+		}
+		String accesToken = jwtTokenProvider.createAccessToken(deviceToken, memberId);
+		return TokenResDto.of(accesToken, refreshToken, LocalDateTime.now().plus(accessExpirationTime, ChronoUnit.MILLIS));
 	}
 
 	private Member kakaoLogin(LoginReqDto loginReqDto) {
