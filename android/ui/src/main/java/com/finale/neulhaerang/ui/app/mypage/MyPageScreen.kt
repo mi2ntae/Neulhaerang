f0cdb42bb1dc4edf64dfd78247838b38c0cf2a2a
kjy6135@naver.com <kjy6135@naver.com>
2023-10-27 16:42:18 +0900

    Design: MyPageScreen 배경색 제거

--- android/ui/src/main/java/com/finale/neulhaerang/ui/app/mypage/MyPageScreen.kt
+++ android/ui/src/main/java/com/finale/neulhaerang/ui/app/mypage/MyPageScreen.kt
@@ -1,10 +1,8 @@
 package com.finale.neulhaerang.ui.app.mypage
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,7 +19,6 @@ fun MyPageScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.secondary)
     ) {
         Column {
             Text("마이페이지입니당")
