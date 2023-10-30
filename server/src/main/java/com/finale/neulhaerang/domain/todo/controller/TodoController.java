4bcdbc280dc08c3f463e4324f3ddf5b8b5752bbe
rladbwls30 <rladbwls30@naver.com>
2023-10-30 16:38:12 +0900

    Feat: Todo 등록 Controller 생성

---
+++ server/src/main/java/com/finale/neulhaerang/domain/todo/controller/TodoController.java
@@ -0,0 +1,35 @@
+package com.finale.neulhaerang.domain.todo.controller;
+
+import java.time.LocalDateTime;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finale.neulhaerang.domain.todo.dto.request.TodoCreateReqDto;
+import com.finale.neulhaerang.domain.todo.entity.Todo;
+import com.finale.neulhaerang.domain.todo.repository.TodoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/todo")
+public class TodoController {
+	private final ModelMapper modelMapper;
+	private final TodoRepository todoRepository;
+
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody @Valid TodoCreateReqDto todoCreateReqDto){
+		Todo todo = modelMapper.map(todoCreateReqDto, Todo.class);
+		todoRepository.save(todo);
+		return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+	}
+}
