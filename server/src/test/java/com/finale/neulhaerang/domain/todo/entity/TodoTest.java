3151691d284e821d5aeadf9d0eeb49a8c8d237d1
rladbwls30 <rladbwls30@naver.com>
2023-10-30 16:42:06 +0900

    Test: Todo Entity 테스트 코드 함수 명 변경

--- server/src/test/java/com/finale/neulhaerang/domain/todo/entity/TodoTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/todo/entity/TodoTest.java
@@ -10,16 +10,17 @@ import com.finale.neulhaerang.domain.routine.entity.StatType;
 class TodoTest {
 	@Test
 	@DisplayName("Todo Entity Build Test")
-	public void builder() {
-		// given // when
+	public void When_EntityBuilder_Expect_isNotNull() {
+		// given, when
 		Todo todo = createTodo("양치하기", StatType.갓생력);
 
 		// then
 		assertThat(todo).isNotNull();
 	}
+
 	@Test
 	@DisplayName("Todo Entity Bean 주입 Test")
-	public void javaBean(){
+	public void When_TodoDI_Expect_isEqualTo(){
 		// given
 		String content = "양치하기";
 		StatType statType = StatType.갓생력;
