126a5021f5d3956e74016d21f9623f6df1d317bb
rladbwls30 <rladbwls30@naver.com>
2023-10-31 12:56:29 +0900

    Test: 빈 값 들어올 경우 예외처리 세분화 및 잘못된 입력 들어올 경우 예외처리 테스트 생성

--- server/src/test/java/com/finale/neulhaerang/domain/todo/controller/TodoControllerTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/todo/controller/TodoControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.finale.neulhaerang.domain.routine.entity.StatType;
 import com.finale.neulhaerang.domain.todo.dto.request.TodoCreateReqDto;
@@ -32,9 +33,9 @@ class TodoControllerTest {
 
 	@Test
 	@DisplayName("Todo 등록 테스트")
-	public void When_InsertTodo_Expect_IsOk() throws Exception {
+	public void When_CreateTodo_Expect_IsOk() throws Exception {
 		// given
-		TodoCreateReqDto todoRequestDto = TodoCreateReqDto.builder()
+		TodoCreateReqDto todoCreateReqDto = TodoCreateReqDto.builder()
 			.todoDate(LocalDateTime.of(2023,11,1,13,30))
 			.content("코딩테스트")
 			.statType(StatType.생존력)
@@ -43,7 +44,7 @@ class TodoControllerTest {
 		// when, then
 		mockMvc.perform(post("/todo/")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(todoRequestDto))
+				.content(objectMapper.writeValueAsString(todoCreateReqDto))
 			)
 			.andDo(print())
 			.andExpect(status().isCreated())
@@ -52,10 +53,13 @@ class TodoControllerTest {
 	}
 
 	@Test
-	@DisplayName("Todo 등록 시 빈 값이 들어올 경우 에러 발생 테스트")
-	public void When_InsertEmptyInput_Expect_BadRequest() throws Exception {
+	@DisplayName("Todo 등록 시 날짜에 빈 값이 들어올 경우 에러 발생 테스트")
+	public void When_InsertEmptyDate_Expect_BadRequest() throws Exception {
 		// given
-		TodoCreateReqDto todoCreateReqDto = TodoCreateReqDto.builder().build();
+		TodoCreateReqDto todoCreateReqDto = TodoCreateReqDto.builder()
+			.content("코딩테스트")
+			.statType(StatType.생존력)
+			.build();
 
 		// when, then
 		mockMvc.perform(post("/todo/")
@@ -68,8 +72,46 @@ class TodoControllerTest {
 	}
 
 	@Test
-	@DisplayName("Todo 등록 시 잘못된 입력값이 등록된 경우 에러 발생 테스트")
-	public void When_InsertWrongInput_Expect_BadRequest() throws Exception{
+	@DisplayName("Todo 등록 시 스탯타입에 빈 값이 들어올 경우 에러 발생 테스트")
+	public void When_InsertEmptyStatType_Expect_BadRequest() throws Exception {
+		// given
+		TodoCreateReqDto todoCreateReqDto = TodoCreateReqDto.builder()
+			.todoDate(LocalDateTime.of(2023,11,1,13,30))
+			.content("코딩테스트")
+			.build();
+
+		// when, then
+		mockMvc.perform(post("/todo/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(todoCreateReqDto))
+			)
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+		;
+	}
+
+	@Test
+	@DisplayName("Todo 등록 시 내용에 빈 값이 들어올 경우 에러 발생 테스트")
+	public void When_InsertEmptyContent_Expect_BadRequest() throws Exception {
+		// given
+		TodoCreateReqDto todoCreateReqDto = TodoCreateReqDto.builder()
+			.todoDate(LocalDateTime.of(2023,11,1,13,30))
+			.statType(StatType.생존력)
+			.build();
+
+		// when, then
+		mockMvc.perform(post("/todo/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(todoCreateReqDto))
+			)
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+		;
+	}
+
+	@Test
+	@DisplayName("Todo 등록 api 요청시 오늘보다 이전 날짜에 등록 요청이 온 경우 에러 반환 테스트")
+	public void When_InsertWrongInput_Expect_BadRequest() throws Exception {
 		// given
 		TodoCreateReqDto todoCreateReqDto = TodoCreateReqDto.builder()
 			.todoDate(LocalDateTime.of(2023,10,1,13,30))
@@ -84,6 +126,8 @@ class TodoControllerTest {
 			)
 			.andDo(print())
 			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("errorCode").value("T-001"))
+			.andExpect(jsonPath("errorMessage").value("등록 날짜가 유효하지 않습니다."))
 		;
 	}
 }
