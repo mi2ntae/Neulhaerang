7b63ef8f9e905e6b1ab3fabdb48401f666b85fa3
rladbwls30 <rladbwls30@naver.com>
2023-10-31 10:14:48 +0900

    Feat: Todo 등록 Service, ServiceImpl 생성

---
+++ server/src/main/java/com/finale/neulhaerang/domain/todo/service/TodoServiceImpl.java
@@ -0,0 +1,27 @@
+package com.finale.neulhaerang.domain.todo.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.finale.neulhaerang.domain.todo.dto.request.TodoCreateReqDto;
+import com.finale.neulhaerang.domain.todo.entity.Todo;
+import com.finale.neulhaerang.domain.todo.repository.TodoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TodoServiceImpl implements TodoService {
+	private final TodoRepository todoRepository;
+
+	private final ModelMapper modelMapper;
+
+	@Override
+	public String createTodo(TodoCreateReqDto todoCreateReqDto) {
+		Todo todo = modelMapper.map(todoCreateReqDto, Todo.class);
+		todoRepository.save(todo);
+		return "OK";
+	}
+}
