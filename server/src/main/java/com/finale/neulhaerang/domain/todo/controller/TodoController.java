c75f6276caf64edc4796b777076017da13c64495
rladbwls30 <rladbwls30@naver.com>
2023-10-31 10:31:21 +0900

    Refactor: Todo 등록 코드 Service로 이동

--- server/src/main/java/com/finale/neulhaerang/domain/todo/controller/TodoController.java
+++ server/src/main/java/com/finale/neulhaerang/domain/todo/controller/TodoController.java
@@ -4,18 +4,15 @@ import java.time.LocalDateTime;
 
 import javax.validation.Valid;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finale.neulhaerang.domain.todo.dto.request.TodoCreateReqDto;
-import com.finale.neulhaerang.domain.todo.entity.Todo;
-import com.finale.neulhaerang.domain.todo.repository.TodoRepository;
+import com.finale.neulhaerang.domain.todo.service.TodoService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,16 +20,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/todo")
 public class TodoController {
-	private final ModelMapper modelMapper;
-	private final TodoRepository todoRepository;
-
+	private final TodoService todoService;
 	@PostMapping
-	public ResponseEntity<?> createTodo(@RequestBody @Valid TodoCreateReqDto todoCreateReqDto){
+	public ResponseEntity<String> createTodo(@RequestBody @Valid TodoCreateReqDto todoCreateReqDto){
 		if(todoCreateReqDto.getTodoDate().isBefore(LocalDateTime.now())){
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Input Date Is Before Today");
 		}
-		Todo todo = modelMapper.map(todoCreateReqDto, Todo.class);
-		todoRepository.save(todo);
-		return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+		return ResponseEntity.status(HttpStatus.CREATED).body(todoService.createTodo(todoCreateReqDto));
 	}
 }
