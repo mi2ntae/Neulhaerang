d79ac74e354ef8c9946ed8f9796e5db11663e584
jungeunevepark <evecomcom98@naver.com>
2023-10-31 17:29:13 +0900

    Test: 필요없는 테스트 코드 제거

--- server/src/test/java/com/finale/neulhaerang/domain/routine/controller/RoutineControllerTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/routine/controller/RoutineControllerTest.java
@@ -51,27 +51,6 @@ class RoutineControllerTest {
 			.andExpect(status().isCreated());
 	}
 
-	@DisplayName("새로운 루틴 생성 시, Alarm는 필수 입니다. 값을 받지 않으면 자동으로 false 지정")
-	@Test
-	@WithUserDetails
-	void When_CreateRoutineWithoutAlarm_Expect_isCreate() throws Exception {
-		// given
-		RoutineCreateReqDto routineCreateReqDto = RoutineCreateReqDto.builder()
-			.content("아침밥 먹고 가기")
-			.repeated(List.of(true, true, true, true, true, true, true))
-			.alarmTime(LocalTime.of(8, 30, 0))
-			.statType(StatType.생존력)
-			.build();
-
-		// when // then
-		mockMvc.perform(post("/routine").with(csrf())
-				.content(objectMapper.writeValueAsString(routineCreateReqDto))
-				.contentType(MediaType.APPLICATION_JSON)
-			)
-			.andDo(print())
-			.andExpect(status().isCreated());
-	}
-
 	@DisplayName("새로운 루틴 생성 시, StatType는 필수 입니다.")
 	@Test
 	@WithUserDetails
