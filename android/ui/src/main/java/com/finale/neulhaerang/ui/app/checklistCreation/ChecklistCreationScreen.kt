c9d08be5ccd418d84219b4f674d605d569c35881
임성원 <dnjsndnjs@gmail.com>
2023-10-31 18:01:28 +0900

    Feat: 체크리스트 하단 완료 버튼

--- android/ui/src/main/java/com/finale/neulhaerang/ui/app/checklistCreation/ChecklistCreationScreen.kt
+++ android/ui/src/main/java/com/finale/neulhaerang/ui/app/checklistCreation/ChecklistCreationScreen.kt
@@ -1,13 +1,18 @@
 package com.finale.neulhaerang.ui.app.checklistCreation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Alarm
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Schedule
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -27,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -41,41 +47,33 @@ import java.time.format.DateTimeFormatter
 fun ChecklistCreationScreen(navController: NavHostController) {
     val viewModel = viewModel<ChecklistCreationViewModel>()
 
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-        TopAppBar(title = {
-            Text(
-                text = "체크리스트 작성"
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "체크리스트 작성"
+                    )
+                }, navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.go_back)
+                        )
+                    }
+                }, colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                )
             )
-        }, navigationIcon = {
-            IconButton(onClick = { navController.popBackStack() }) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowBack,
-                    contentDescription = stringResource(id = R.string.go_back)
-                )
-            }
-        }, actions = {
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(
-                    text = stringResource(id = R.string.cancel),
-                    color = MaterialTheme.colorScheme.secondary
-                )
-            }
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(
-                    text = stringResource(id = R.string.complete),
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
-        }, colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-        )
-        )
-    }) {
+        }
+    ) {
         Content(
             modifier = Modifier
                 .padding(paddingValues = it)
+                .padding(all = 16.dp)
                 .fillMaxSize(),
             viewModel = viewModel
         )
@@ -93,6 +91,7 @@ fun Content(modifier: Modifier = Modifier, viewModel: ChecklistCreationViewModel
             stat = viewModel.stat.value,
             changeStat = viewModel::changeStat
         )
+        Spacer(modifier = Modifier.height(8.dp))
         ChecklistCreationItem(
             name = stringResource(id = R.string.checklist_category_routine),
             icon = Icons.Filled.Refresh
@@ -129,6 +128,17 @@ fun Content(modifier: Modifier = Modifier, viewModel: ChecklistCreationViewModel
             icon = Icons.Filled.Alarm
         ) {
             Switch(checked = viewModel.alarm.value, onCheckedChange = viewModel::changeAlarm)
+        }
+        Spacer(modifier = Modifier.weight(weight = 1f))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.complete),
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
