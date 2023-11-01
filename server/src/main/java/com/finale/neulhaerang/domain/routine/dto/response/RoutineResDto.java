4eee760feab33b17f4578f101b68185c9108c5e2
jungeunevepark <evecomcom98@naver.com>
2023-11-01 17:52:23 +0900

    Feat: RoutineResDto 생성

---
+++ server/src/main/java/com/finale/neulhaerang/domain/routine/dto/response/RoutineResDto.java
@@ -0,0 +1,9 @@
+package com.finale.neulhaerang.domain.routine.dto.response;
+
+import java.time.LocalDate;
+
+public class RoutineResDto {
+	private Long routineId;
+	private String content;
+	private LocalDate date;
+}
