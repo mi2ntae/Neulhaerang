549edcbdf70742e1cbadc00808aa28a1ccc851a1
jungeunevepark <evecomcom98@naver.com>
2023-11-01 16:56:29 +0900

    Feat: 해당 날짜에 실행해야하는 루틴을 dailyRoutine에 저장하는 함수 구현

--- server/src/main/java/com/finale/neulhaerang/domain/routine/service/RoutineScheduler.java
+++ server/src/main/java/com/finale/neulhaerang/domain/routine/service/RoutineScheduler.java
@@ -1,10 +1,14 @@
 package com.finale.neulhaerang.domain.routine.service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.finale.neulhaerang.domain.routine.entity.DailyRoutine;
+import com.finale.neulhaerang.domain.routine.entity.Routine;
+import com.finale.neulhaerang.domain.routine.repository.DailyRoutineRepository;
 import com.finale.neulhaerang.domain.routine.repository.RoutineRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 public class RoutineScheduler {
 
 	private final RoutineRepository routineRepository;
+	private final DailyRoutineRepository dailyRoutineRepository;
 
 	@Scheduled(cron = "${schedules.cron.daily-routine}", zone = "Asia/Seoul")
 	public void createDailyRoutineTrigger() {
@@ -23,7 +28,17 @@ public class RoutineScheduler {
 	}
 
 	void createDailyRoutine(LocalDate date) {
-		// 데일리 루틴 생성
+		// date에 해야하는 루틴들을 가져옵니다.
+		StringBuilder dayOfVaule = new StringBuilder("_______");
+		int dayOfWeekValue = date.getDayOfWeek().getValue() - 1;
+		dayOfVaule.setCharAt(dayOfWeekValue, '1');
+		List<Routine> routinesOfDay = routineRepository.findRoutinesByDayOfValue(dayOfVaule.toString(), date);
+		System.out.println(routinesOfDay);
+
+		// dailyRoutine에 저장합니다.
+		routinesOfDay.forEach(r -> {
+			dailyRoutineRepository.save(DailyRoutine.create(r, date));
+		});
 	}
 
 }
