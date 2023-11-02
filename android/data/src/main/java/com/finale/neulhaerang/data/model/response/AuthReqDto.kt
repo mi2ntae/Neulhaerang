e6c57cbfc7ecf826ee2082f21d1157cbf1b1f9c0
kosigi19 <kosigi19@naver.com>
2023-11-02 14:17:40 +0900

    Feat: LoginResDto 추가

---
+++ android/data/src/main/java/com/finale/neulhaerang/data/model/response/AuthReqDto.kt
@@ -0,0 +1,11 @@
+package com.finale.neulhaerang.data.model.response
+
+import java.time.LocalDateTime
+
+data class LoginResDto(
+    val memberId: Long,
+    val nickname: String,
+    val accessToken: String,
+    val refreshToken: String,
+    val expiredTime: String,
+)
