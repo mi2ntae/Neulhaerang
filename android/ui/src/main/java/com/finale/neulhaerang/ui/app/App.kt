822868bf6b09f20f1040189de93e17cae72a3b9b
kosigi19 <kosigi19@naver.com>
2023-11-02 09:28:53 +0900

    Feat: 로그인 시, 메인화면으로 이동 확인 중

--- android/ui/src/main/java/com/finale/neulhaerang/ui/app/App.kt
+++ android/ui/src/main/java/com/finale/neulhaerang/ui/app/App.kt
@@ -1,14 +1,17 @@
 package com.finale.neulhaerang.ui.app
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.finale.neulhaerang.common.navigation.AppNavItem
+import com.finale.neulhaerang.domain.KakaoAuthViewModel
 import com.finale.neulhaerang.ui.app.checklistCreation.ChecklistCreationScreen
 import com.finale.neulhaerang.ui.app.login.LoginScreen
 import com.finale.neulhaerang.ui.app.main.MainScreen
@@ -19,15 +22,18 @@ import com.finale.neulhaerang.ui.theme.NeulHaeRangTheme
 /**
  * 메인 앱
  */
+@SuppressLint("StateFlowValueCalledInComposition")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
     val navController = rememberNavController()
 
+    val viewModel = viewModel<KakaoAuthViewModel>()
+
     NeulHaeRangTheme {
         NavHost(
             navController = navController,
-            startDestination = AppNavItem.Login.route,
+            startDestination = if(viewModel.isLoggedIn.value) AppNavItem.Main.route else AppNavItem.Login.route,
             modifier = Modifier.fillMaxSize(),
         ) {
             AppNavItem.values().forEach { item ->
