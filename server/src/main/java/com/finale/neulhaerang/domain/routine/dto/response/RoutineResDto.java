96eb73d5363e417939aa268d77c6dcd3ad5b0e94
jungeunevepark <evecomcom98@naver.com>
2023-11-02 09:43:58 +0900

    Feat: Routine을 RoutineResDto로 변환하는 정적 팩토리 메서드 구현

--- server/src/main/java/com/finale/neulhaerang/domain/routine/dto/response/RoutineResDto.java
+++ server/src/main/java/com/finale/neulhaerang/domain/routine/dto/response/RoutineResDto.java
@@ -1,9 +1,24 @@
 package com.finale.neulhaerang.domain.routine.dto.response;
 
-import java.time.LocalDate;
+import com.finale.neulhaerang.domain.routine.entity.Routine;
+
+import lombok.Builder;
 
 public class RoutineResDto {
 	private Long routineId;
+
 	private String content;
-	private LocalDate date;
+
+	@Builder
+	private RoutineResDto(Long routineId, String content) {
+		this.routineId = routineId;
+		this.content = content;
+	}
+
+	public static RoutineResDto from(Routine routine) {
+		return RoutineResDto.builder()
+			.routineId(routine.getId())
+			.content(routine.getContent())
+			.build();
+	}
 }
