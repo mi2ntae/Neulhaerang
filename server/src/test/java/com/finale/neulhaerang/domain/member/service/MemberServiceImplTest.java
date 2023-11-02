9e5f17647b2e0e858da358a6bce6865c0f8a9483
김민태 <kimmintae981019@gmail.com>
2023-11-02 09:41:10 +0900

    Test: 회원 상태 정보 조회하기 테스트 케이스 추가

--- server/src/test/java/com/finale/neulhaerang/domain/member/service/MemberServiceImplTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/member/service/MemberServiceImplTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.finale.neulhaerang.domain.member.dto.response.MemberStatusResDto;
 import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.member.repository.MemberRepository;
 import com.finale.neulhaerang.global.exception.member.NotExistMemberException;
@@ -23,7 +24,26 @@ class MemberServiceImplTest {
 	private MemberRepository memberRepository;
 
 	@Test
-	void findStatusByMemberId() {
+	@DisplayName("회원 상태 정보를 조회할 경우 MemberStatusResDto 형식으로 결과를 반환한다.")
+	void When_FindMemberStatus_Expect_MemberStatusDto() {
+		// given when
+		Member member = memberRepository.save(createMember());
+
+		// then
+		assertThat(memberService.findStatusByMemberId(member.getId())).isInstanceOf(MemberStatusResDto.class);
+	}
+
+	@Test
+	@DisplayName("회원 상태 정보를 조회할 경우 MemberStatusResDto 형식으로 결과를 반환한다.")
+	void When_FindNotExistMemberStatus_Expect_ThrowException() {
+		// given when
+		Member member = memberRepository.save(createMember());
+
+		// when
+		long memberId = member.getId()-1; // 0
+
+		// then
+		assertThatThrownBy(() -> memberService.findStatusByMemberId(memberId)).isInstanceOf(NotExistMemberException.class);
 	}
 
 	@Test
@@ -37,7 +57,7 @@ class MemberServiceImplTest {
 
 	@Test
 	@DisplayName("회원 탈퇴를 진행할 경우, Member의 withdrawalDate에 값이 설정된다.")
-	void When_RemoveMember_WithdrawalDateIsNotNull() {
+	void When_RemoveMember_Expect_WithdrawalDateIsNotNull() {
 		// given
 		Member member = memberRepository.save(createMember());
 
@@ -50,7 +70,7 @@ class MemberServiceImplTest {
 
 	@Test
 	@DisplayName("회원 탈퇴를 진행할 경우, 존재하지 않는 멤버를 삭제하면 에러가 난다.")
-	void When_RemoveNotExistMember_ThrowException() {
+	void When_RemoveNotExistMember_Expect_ThrowException() {
 		// given
 		Member member = memberRepository.save(createMember());
 
