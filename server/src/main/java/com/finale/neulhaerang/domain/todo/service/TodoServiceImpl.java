38f27edc8f05c0ee0ed429856413407d776a588c
rladbwls30 <rladbwls30@naver.com>
2023-11-02 09:34:38 +0900

    Feat: Todo List 조회하는 ServiceImpl 로직 생성

--- server/src/main/java/com/finale/neulhaerang/domain/todo/service/TodoServiceImpl.java
+++ server/src/main/java/com/finale/neulhaerang/domain/todo/service/TodoServiceImpl.java
@@ -1,6 +1,10 @@
 package com.finale.neulhaerang.domain.todo.service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.member.repository.MemberRepository;
 import com.finale.neulhaerang.domain.todo.dto.request.TodoCreateReqDto;
+import com.finale.neulhaerang.domain.todo.dto.response.TodoListResDto;
 import com.finale.neulhaerang.domain.todo.entity.Todo;
 import com.finale.neulhaerang.domain.todo.repository.TodoRepository;
 import com.finale.neulhaerang.global.exception.todo.InvalidTodoDateException;
@@ -30,5 +35,24 @@ public class TodoServiceImpl implements TodoService {
 		}
 		Member member = memberRepository.getReferenceById(authenticationHandler.getLoginMemberId());
 		todoRepository.save(Todo.create(todoCreateReqDto, member));
+	}
+
+	@Override
+	public List<TodoListResDto> findTodo(LocalDate todoDate) {
+		Member member = memberRepository.getReferenceById(authenticationHandler.getLoginMemberId());
+
+		LocalDateTime startDate = todoDate.atStartOfDay();
+		LocalDateTime endDate = todoDate.atTime(LocalTime.MAX);
+
+		List<Todo> todoList = todoRepository.findTodosByMemberAndTodoDateIsBetween(member, startDate, endDate);
+		List<TodoListResDto> todoResDtoList = new ArrayList<>();
+		for (Todo todo : todoList) {
+			String alarmTime = todo.getTodoDate().getHour()+":"+todo.getTodoDate().getMinute();
+			todoResDtoList.add(TodoListResDto.of(todo, alarmTime));
+		}
+		if(todoResDtoList.size()==0){
+			return null;
+		}
+		return todoResDtoList;
 	}
 }
