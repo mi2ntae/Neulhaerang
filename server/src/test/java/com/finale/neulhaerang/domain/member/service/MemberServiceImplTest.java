afd179600e85f8f51a685a6f6edfe3a8a29b5f2e
김민태 <kimmintae981019@gmail.com>
2023-11-02 13:26:45 +0900

    Test: 멤버 스탯 전체 조회 테스트 케이스 작성

--- server/src/test/java/com/finale/neulhaerang/domain/member/service/MemberServiceImplTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/member/service/MemberServiceImplTest.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.finale.neulhaerang.domain.member.dto.response.MemberCharacterResDto;
 import com.finale.neulhaerang.domain.member.dto.response.MemberStatusResDto;
+import com.finale.neulhaerang.domain.member.dto.response.StatListResDto;
 import com.finale.neulhaerang.domain.member.entity.CharacterInfo;
 import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.member.repository.CharacterInfoRepository;
@@ -39,7 +40,7 @@ class MemberServiceImplTest {
 	}
 
 	@Test
-	@DisplayName("회원 상태 정보를 조회할 경우 MemberStatusResDto 형식으로 결과를 반환한다.")
+	@DisplayName("회원 상태 정보를 조회할 경우, MemberStatusResDto 형식으로 결과를 반환한다.")
 	void When_FindMemberStatus_Expect_MemberStatusDto() {
 		// given when
 		Member member = this.testMember;
@@ -49,9 +50,9 @@ class MemberServiceImplTest {
 	}
 
 	@Test
-	@DisplayName("존재하지 않는 회원 상태 정보를 조회할 경우 예외를 발생시킨다.")
+	@DisplayName("존재하지 않는 회원 상태 정보를 조회할 경우, 예외를 발생시킨다.")
 	void When_FindNotExistMemberStatus_Expect_ThrowException() {
-		// given when
+		// given
 		Member member = this.testMember;
 
 		// when
@@ -62,7 +63,7 @@ class MemberServiceImplTest {
 	}
 
 	@Test
-	@DisplayName("회원 캐릭터 정보를 조회할 경우 MemberCharacterResDto 형식으로 결과를 반환하고 저장된 캐릭터 정보를 가진다.")
+	@DisplayName("회원 캐릭터 정보를 조회할 경우, MemberCharacterResDto 형식으로 결과를 반환하고 저장된 캐릭터 정보를 가진다.")
 	void When_FindMemberCharacterInfo_Expect_MemberCharacterDto() {
 		// given
 		Member member = this.testMember;
@@ -79,7 +80,7 @@ class MemberServiceImplTest {
 	}
 
 	@Test
-	@DisplayName("캐릭터 정보가 등록되지 않은 회원의 캐릭터 정보를 조회할 경우 예외를 발생시킨다.")
+	@DisplayName("캐릭터 정보가 등록되지 않은 회원의 캐릭터 정보를 조회할 경우, 예외를 발생시킨다.")
 	void When_FindMemberNotExistCharacterInfo_Expect_ThrowException() {
 		// given when
 		Member member = this.testMember;
@@ -117,6 +118,32 @@ class MemberServiceImplTest {
 
 		// then
 		assertThatThrownBy(() -> memberService.removeMember()).isInstanceOf(NotExistMemberException.class);
+	}
+
+	@Test
+	@DisplayName("회원 스탯 전체 조회를 진행할 경우, List<StatListResDto> 형식으로 결과를 반환하며 길이가 6이다.")
+	void When_FindStats_Expect_StatListResDto() {
+		// given when
+		Member member = this.testMember;
+
+		// then
+		assertSoftly(s -> {
+			s.assertThat(memberService.findAllStatsByMemberId(member.getId())).hasSize(6);
+			s.assertThat(memberService.findAllStatsByMemberId(member.getId()).get(0)).isInstanceOf(StatListResDto.class);
+		});
+	}
+
+	@Test
+	@DisplayName("회원 스탯 전체 조회를 진행할 경우, 존재하지 않는 멤버의 스탯을 조회하면 예외를 발생시킨다.")
+	void When_FindStatsNotExistMember_Expect_ThrowException() {
+		// given
+		Member member = this.testMember;
+
+		// when
+		long memberId = member.getId()-1;
+
+		// then
+		assertThatThrownBy(() -> memberService.findAllStatsByMemberId(memberId)).isInstanceOf(NotExistMemberException.class);
 	}
 
 	private static Member createMember() {
