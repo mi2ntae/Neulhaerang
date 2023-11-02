09101f08df63abdef095d043d7cc87e95586d990
임성원 <dnjsndnjs@gmail.com>
2023-11-02 15:49:30 +0900

    Feat: 안드로이드 APIs ResponseResultCall 정의

---
+++ android/data/src/main/java/com/finale/neulhaerang/data/util/ResponseResultCall.kt
@@ -0,0 +1,70 @@
+package com.finale.neulhaerang.data.util
+
+import android.util.Log
+import okhttp3.Request
+import okio.Timeout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ResponseResultCall<T>(
+    private val call: Call<T>,
+) : Call<ResponseResult<T>> {
+    override fun clone(): Call<ResponseResult<T>> = ResponseResultCall(call.clone())
+
+    override fun execute(): Response<ResponseResult<T>> {
+        throw UnsupportedOperationException("ResponseResultCall does not support execute")
+    }
+
+    override fun isExecuted(): Boolean = call.isExecuted
+
+    override fun cancel(): Unit = call.cancel()
+
+    override fun isCanceled(): Boolean = call.isCanceled
+
+    override fun request(): Request = call.request()
+
+    override fun timeout(): Timeout = call.timeout()
+
+    override fun enqueue(callback: Callback<ResponseResult<T>>) {
+        val TAG = "ResponseResultCall"
+
+        call.enqueue(object : Callback<T> {
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                val body = response.body()
+//                val message = response.message()
+                val error = response.errorBody()
+                val code = response.code()
+
+                if (response.isSuccessful) {
+                    // 성공
+                    if (body != null) {
+                        // null이 아닌 경우
+                        callback.onResponse(
+                            this@ResponseResultCall,
+                            Response.success(ResponseResult.Success(code, body))
+                        )
+                    } else {
+                        // null인 경우
+                        callback.onResponse(
+                            this@ResponseResultCall,
+//                            Response.success(ResponseResult.Failure(code, message, null))
+                            Response.success(ResponseResult.NullSuccess(code))
+                        )
+                    }
+                } else {
+                    // 실패
+                    val errorBody = error!!.string()
+                    callback.onResponse(
+                        this@ResponseResultCall,
+                        Response.success(ResponseResult.Failure(code, errorBody, null))
+                    )
+                }
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+}
