715a9b27abbdbba954d78474ce208a4e10e71b8b
jungeunevepark <evecomcom98@naver.com>
2023-11-02 17:43:47 +0900

    Feat: AllArgConstructor 생성

--- server/src/main/java/com/finale/neulhaerang/domain/routine/dto/request/RoutineRemoveReqDto.java
+++ server/src/main/java/com/finale/neulhaerang/domain/routine/dto/request/RoutineRemoveReqDto.java
@@ -2,10 +2,23 @@ package com.finale.neulhaerang.domain.routine.dto.request;
 
 import javax.validation.constraints.NotNull;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class RoutineRemoveReqDto {
 	@NotNull
 	private Long dailyRoutineId;
 	@NotNull
 	private Long routineId;
 	private boolean never;
+
+	@Builder
+	private RoutineRemoveReqDto(Long dailyRoutineId, Long routineId, boolean never) {
+		this.dailyRoutineId = dailyRoutineId;
+		this.routineId = routineId;
+		this.never = never;
+	}
 }
