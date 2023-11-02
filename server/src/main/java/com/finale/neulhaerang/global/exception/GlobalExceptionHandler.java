5cc608db6eb52d0f662a33807356c0c64d95e15e
rladbwls30 <rladbwls30@naver.com>
2023-11-02 14:07:49 +0900

    Remove: 필요없어진 에러 코드 삭제

--- server/src/main/java/com/finale/neulhaerang/global/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/finale/neulhaerang/global/exception/GlobalExceptionHandler.java
@@ -17,7 +17,6 @@ import com.finale.neulhaerang.global.exception.common.NotExistAlarmTimeException
 import com.finale.neulhaerang.global.exception.member.NotExistCharacterInfoException;
 import com.finale.neulhaerang.global.exception.member.NotExistDeviceException;
 import com.finale.neulhaerang.global.exception.member.NotExistMemberException;
-import com.finale.neulhaerang.global.exception.todo.AlreadyRemoveTodoException;
 import com.finale.neulhaerang.global.exception.todo.InvalidTodoDateException;
 import com.finale.neulhaerang.global.exception.todo.NotExistTodoException;
 
@@ -50,15 +49,6 @@ public class GlobalExceptionHandler {
 		ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.NOT_EXIST_TODO.getErrorCode(),
 			ErrorCode.NOT_EXIST_TODO.getMessage());
 		return ResponseEntity.status(ErrorCode.NOT_EXIST_TODO.getHttpStatus())
-			.body(errorResponse);
-	}
-
-	@ExceptionHandler(AlreadyRemoveTodoException.class)
-	protected ResponseEntity<ErrorResponse> alreadyRemoveTodoException() {
-		log.error("todo is already removed");
-		ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.ALREADY_REMOVE_TODO.getErrorCode(),
-			ErrorCode.ALREADY_REMOVE_TODO.getMessage());
-		return ResponseEntity.status(ErrorCode.ALREADY_REMOVE_TODO.getHttpStatus())
 			.body(errorResponse);
 	}
 
