58baa2ce059781c94838d92af4fcabd9d9fcacd6
jungeunevepark <evecomcom98@naver.com>
2023-11-02 15:52:11 +0900

    Fix: 파라미터 값 Routine으로 변경

--- server/src/main/java/com/finale/neulhaerang/global/exception/routine/AlreadyRemoveRoutineException.java
+++ server/src/main/java/com/finale/neulhaerang/global/exception/routine/AlreadyRemoveRoutineException.java
@@ -1,7 +1,7 @@
 package com.finale.neulhaerang.global.exception.routine;
 
 import com.finale.neulhaerang.domain.member.entity.Member;
-import com.finale.neulhaerang.domain.routine.entity.DailyRoutine;
+import com.finale.neulhaerang.domain.routine.entity.Routine;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,12 +11,12 @@ public class AlreadyRemoveRoutineException extends RuntimeException {
 		super();
 	}
 
-	public AlreadyRemoveRoutineException(Member member, DailyRoutine dailyRoutine) {
+	public AlreadyRemoveRoutineException(Member member, Routine routine) {
 		super();
 		log.error(
 			member.getNickname() + "(member_id=" + member.getId()
-				+ ")님이 dailyRoutine(daily_routine_id=" + dailyRoutine.getId() + ", content=" + dailyRoutine.getRoutine()
-				.getContent() + ")의 check 값을 변경하려 하셨는데 해당 daily routine은 이미 삭제되었습니다.");
+				+ ")님이 dailyRoutine(daily_routine_id=" + routine.getId() + ", content=" + routine.getContent()
+				+ ")의 check 값을 변경하려 하셨는데 해당 daily routine은 이미 삭제되었습니다.");
 	}
 
 	public AlreadyRemoveRoutineException(String message) {
