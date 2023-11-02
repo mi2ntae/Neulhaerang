5408de581cbcd52179b461c180fa184e911cc302
rladbwls30 <rladbwls30@naver.com>
2023-11-02 09:32:48 +0900

    Test: Todo 리스트 제대로 조회하는 테스트 ㅅ작성

--- server/src/test/java/com/finale/neulhaerang/domain/todo/service/TodoServiceTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/todo/service/TodoServiceTest.java
@@ -66,6 +66,34 @@ class TodoServiceTest extends BaseTest {
 			.isInstanceOf(InvalidTodoDateException.class);
 	}
 
+	@Test
+	@DisplayName("해당 날짜에 Todo가 있을 경우 해당 날짜의 Todo만 리스트로 조회하는 테스트")
+	public void When_FindTodo_Expect_TodoList() {
+		// given
+		Todo todo1 = this.createTodo("코딩테스트",StatType.생존력,LocalDateTime.of(2023,11,5,10,30));
+		Todo todo2 = this.createTodo("찬구랑 점심먹기",StatType.인싸력,LocalDateTime.of(2023,11,1,12,30));
+		Todo todo3 = this.createTodo("양치하기",StatType.갓생력,LocalDateTime.of(2023,11,1,13,30));
+		Todo todo4 = this.createTodo("산책가기",StatType.튼튼력,LocalDateTime.of(2023,11,1,14,50));
+		todoRepository.saveAll(List.of(todo1, todo2, todo3, todo4));
+
+		LocalDate todoDate = LocalDate.of(2023,11,1);
+
+		// when
+		List<TodoListResDto> todoList = todoService.findTodo(todoDate);
+
+		// then
+		assertThat(todoList).hasSize(3)
+			.extracting("content","statType","alarm","check","alarmTime")
+			.containsExactly(
+				tuple("찬구랑 점심먹기",StatType.인싸력,false,false,"12:30"),
+				tuple("양치하기",StatType.갓생력,false,false,"13:30"),
+				tuple("산책가기",StatType.튼튼력,false,false,"14:50")
+			)
+			.doesNotContain(
+				tuple("코딩테스트",StatType.생존력,false,false,"10:30")
+			);
+	}
+
 	private Todo createTodo(String content, StatType statType, LocalDateTime todoDate){
 		return Todo.builder()
 			.member(member)
