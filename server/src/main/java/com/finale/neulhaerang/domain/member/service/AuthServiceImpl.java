ed6e783d6ef744dcee49b7308fcee0a86e8c29d0
jungeunevepark <evecomcom98@naver.com>
2023-11-03 16:49:17 +0900

    Feat: 회원가입 이벤트 추가

--- server/src/main/java/com/finale/neulhaerang/domain/member/service/AuthServiceImpl.java
+++ server/src/main/java/com/finale/neulhaerang/domain/member/service/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import com.finale.neulhaerang.domain.member.dto.request.LoginReqDto;
@@ -17,6 +18,7 @@ import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.member.feignclient.KakaoInfoFeignClient;
 import com.finale.neulhaerang.domain.member.repository.DeviceRepository;
 import com.finale.neulhaerang.domain.member.repository.MemberRepository;
+import com.finale.neulhaerang.global.event.RegisteredEvent;
 import com.finale.neulhaerang.global.exception.common.ExpiredAuthException;
 import com.finale.neulhaerang.global.exception.common.NotValidJwtTokenException;
 import com.finale.neulhaerang.global.util.AuthenticationHandler;
@@ -29,11 +31,12 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class AuthServiceImpl implements AuthService{
+public class AuthServiceImpl implements AuthService {
 	private final MemberRepository memberRepository;
 	private final DeviceRepository deviceRepository;
 	private final JwtTokenProvider jwtTokenProvider;
 	private final AuthenticationHandler authenticationHandler;
+	private final ApplicationEventPublisher applicationEventPublisher;
 	private final RedisUtil redisUtil;
 
 	// Feign Client
@@ -69,14 +72,14 @@ public class AuthServiceImpl implements AuthService{
 		NotValidJwtTokenException, ExpiredAuthException {
 		String deviceToken = tokenReqDto.getDeviceToken();
 		Optional<Device> optionalDevice = deviceRepository.findDeviceByDeviceToken(deviceToken);
-		if(optionalDevice.isEmpty()) {
+		if (optionalDevice.isEmpty()) {
 			throw new ExpiredAuthException();
 		}
 
 		String refreshToken = tokenReqDto.getRefreshToken();
 		String savedRefreshToken = redisUtil.getData(deviceToken);
-		if(savedRefreshToken != null)  {
-			if(!savedRefreshToken.equals(refreshToken)) {
+		if (savedRefreshToken != null) {
+			if (!savedRefreshToken.equals(refreshToken)) {
 				// 해당 디바이스로 저장된 리프레쉬 토큰이 아닐 때 -> 변조 가능성
 				throw new NotValidJwtTokenException();
 			}
@@ -85,7 +88,8 @@ public class AuthServiceImpl implements AuthService{
 			throw new ExpiredAuthException();
 		}
 		String accessToken = jwtTokenProvider.createAccessToken(deviceToken, optionalDevice.get().getMember().getId());
-		return TokenResDto.of(accessToken, tokenReqDto.getRefreshToken(), LocalDateTime.now().plus(accessExpirationTime, ChronoUnit.MILLIS));
+		return TokenResDto.of(accessToken, tokenReqDto.getRefreshToken(),
+			LocalDateTime.now().plus(accessExpirationTime, ChronoUnit.MILLIS));
 	}
 
 	@Override
@@ -96,17 +100,21 @@ public class AuthServiceImpl implements AuthService{
 	}
 
 	private Member kakaoLogin(LoginReqDto loginReqDto) {
-		KakaoUserResDto kakaoUserResDto = kakaoInfoFeignClient.getKakaoUserInfo("Bearer "+loginReqDto.getAccessToken(), "application/x-www-form-urlencoded");
-		Optional<Member> optionalMember = memberRepository.findMemberByKakaoIdAndWithdrawalDateIsNull(kakaoUserResDto.getId());
+		KakaoUserResDto kakaoUserResDto = kakaoInfoFeignClient.getKakaoUserInfo(
+			"Bearer " + loginReqDto.getAccessToken(), "application/x-www-form-urlencoded");
+		Optional<Member> optionalMember = memberRepository.findMemberByKakaoIdAndWithdrawalDateIsNull(
+			kakaoUserResDto.getId());
 		Optional<Device> optionalDevice = deviceRepository.findDeviceByDeviceToken(loginReqDto.getDeviceToken());
-		if(optionalMember.isEmpty()) {
-			Member member = memberRepository.save(Member.create(kakaoUserResDto.getId(), kakaoUserResDto.getKakao_account().getProfile().getNickname()));
-			if(optionalDevice.isEmpty()) {
+		if (optionalMember.isEmpty()) {
+			Member member = memberRepository.save(
+				Member.create(kakaoUserResDto.getId(), kakaoUserResDto.getKakao_account().getProfile().getNickname()));
+			if (optionalDevice.isEmpty()) {
 				deviceRepository.save(Device.create(member, loginReqDto.getDeviceToken()));
 			}
+			applicationEventPublisher.publishEvent(new RegisteredEvent(member));
 			return member;
 		} else {
-			if(optionalDevice.isEmpty()) {
+			if (optionalDevice.isEmpty()) {
 				deviceRepository.save(Device.create(optionalMember.get(), loginReqDto.getDeviceToken()));
 			}
 			return optionalMember.get();
