944f84975399dad6a8553f74fa6113107c9371cf
임성원 <dnjsndnjs@gmail.com>
2023-11-03 13:09:34 +0900

    Feat: 안드로이드 체크리스트 retrofit 로깅 인터셉터 추가

--- android/data/src/main/java/com/finale/neulhaerang/data/api/Api.kt
+++ android/data/src/main/java/com/finale/neulhaerang/data/api/Api.kt
@@ -1,10 +1,14 @@
 package com.finale.neulhaerang.data.api
 
+import android.util.Log
 import com.finale.neulhaerang.data.util.GsonDateFormatAdapter
 import com.finale.neulhaerang.data.util.GsonTimeFormatAdapter
 import com.finale.neulhaerang.data.util.ResponseResultAdapter
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.ihsanbal.logging.Level
+import com.ihsanbal.logging.LoggingInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -18,11 +22,26 @@ import java.time.LocalTime
  */
 interface Api {
     companion object {
+        // 서버 주소
         private const val BASE_URL = "http://k9a502.p.ssafy.io/api/"
+
+        // JSON 파싱용 Gson
         private val gson: Gson = GsonBuilder().setLenient()
             .registerTypeAdapter(LocalDateTime::class.java, GsonDateFormatAdapter())
             .registerTypeAdapter(LocalTime::class.java, GsonTimeFormatAdapter())
             .create()
+
+        // 디버깅 인터셉터
+        private val debugInterceptor = LoggingInterceptor.Builder()
+            .setLevel(Level.BASIC)
+            .log(Log.DEBUG)
+            .build()
+
+        // OkHttpClient
+        private val client = OkHttpClient.Builder()
+            .addInterceptor(debugInterceptor)
+            .build()
+
 
         private fun create(): Retrofit {
             return Retrofit.Builder()
@@ -30,6 +49,7 @@ interface Api {
                 .addCallAdapterFactory(ResponseResultAdapter.Factory())
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
                 .build()
         }
 
