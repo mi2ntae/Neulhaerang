fdbd551b328e0bf9881ccb0a576843cfea331d22
임성원 <dnjsndnjs@gmail.com>
2023-11-06 14:24:28 +0900

    Feat: 안드로이드 메인페이지 체크리스트 체크시 취소선

--- android/ui/src/main/java/com/finale/neulhaerang/ui/app/main/CheckList.kt
+++ android/ui/src/main/java/com/finale/neulhaerang/ui/app/main/CheckList.kt
@@ -17,9 +17,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -96,7 +98,9 @@ fun CheckListItem(item: CheckList) {
                     lineHeightStyle = LineHeightStyle(
                         alignment = LineHeightStyle.Alignment.Center,
                         trim = LineHeightStyle.Trim.FirstLineTop
-                    )
+                    ),
+                    color = if (isCompleted) Color.Gray else TextStyle.Default.color,
+                    textDecoration = if (isCompleted) TextDecoration.LineThrough else null
                 )
             )
         )
