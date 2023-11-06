0222f9e06739a4f9267037da7f643f5b52193a54
rladbwls30 <rladbwls30@naver.com>
2023-11-06 14:46:48 +0900

    Feat: 해당 날짜에 편지 불러온 후 읽음 처리까지 하는 Service Impl 함수 생성

---
+++ server/src/main/java/com/finale/neulhaerang/domain/letter/service/LetterServiceImpl.java
@@ -0,0 +1,34 @@
+package com.finale.neulhaerang.domain.letter.service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.finale.neulhaerang.domain.letter.entity.Letter;
+import com.finale.neulhaerang.domain.letter.repository.LetterRepository;
+import com.finale.neulhaerang.domain.member.entity.Member;
+import com.finale.neulhaerang.domain.member.repository.MemberRepository;
+import com.finale.neulhaerang.global.exception.member.NotExistMemberException;
+import com.finale.neulhaerang.global.util.AuthenticationHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LetterServiceImpl implements LetterService {
+	private final LetterRepository letterRepository;
+	private final AuthenticationHandler authenticationHandler;
+	private final MemberRepository memberRepository;
+
+	@Override
+	public String findDailyLetter(LocalDate date) {
+		Member member = memberRepository.findById(authenticationHandler.getLoginMemberId())
+			.orElseThrow(NotExistMemberException::new);
+		Optional<Letter> letter = letterRepository.findLetterByMemberAndLetterDate(member, date);
+		letter.ifPresent(Letter::updateRead);
+		return letter.map(Letter::getContent).orElse(null);
+	}
+}
