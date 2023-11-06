c724c2de034e8eeaf9c221e1bfcf8507b3b4d689
jungeunevepark <evecomcom98@naver.com>
2023-11-06 16:25:15 +0900

    Test: StatTitle 획득 함수의 예외 및 다른 스탯 관련 테스트 코드 작성

--- server/src/test/java/com/finale/neulhaerang/global/event/handler/StatTitleEventHandlerTest.java
+++ server/src/test/java/com/finale/neulhaerang/global/event/handler/StatTitleEventHandlerTest.java
@@ -18,6 +18,7 @@ import com.finale.neulhaerang.domain.title.entity.EarnedTitle;
 import com.finale.neulhaerang.domain.title.entity.Title;
 import com.finale.neulhaerang.domain.title.repository.EarnedTitleRepository;
 import com.finale.neulhaerang.domain.title.repository.TitleRepository;
+import com.finale.neulhaerang.global.exception.title.NotExistTitleException;
 import com.finale.neulhaerang.global.util.BaseTest;
 
 class StatTitleEventHandlerTest extends BaseTest {
@@ -156,6 +157,190 @@ class StatTitleEventHandlerTest extends BaseTest {
 		// then
 		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
 		assertThat(earnedTitles).hasSize(0);
+	}
+
+	@DisplayName("칭호 발급 시, 해당 칭호가 존재하지 않으면 에러가 납니다.")
+	@Test
+	void When_LifeStat50Point_Expect_GetNothingLifeStatTitle() {
+		// given
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("늘해랑 가입 환영", LocalDateTime.now().minusDays(20), StatType.갓생력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("멋쟁이 토마토", LocalDateTime.now().minusDays(17), StatType.갓생력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("나야나", LocalDateTime.now().minusDays(1), StatType.갓생력, 10),
+				member.getId()));
+
+		// when // then
+		assertThatThrownBy(() -> statTitleEventHandler.checkIfGetTitleOrNot(member, StatType.갓생력))
+			.isInstanceOf(NotExistTitleException.class);
+	}
+
+	@DisplayName("생존력의 stat 포인트가 90이라면, 생존력 관련 모든 칭호가 발급됩니다.")
+	@Test
+	void When_SurvivalStatMoreThan90_Expect_GetAllOfSurvivalStatTitle() {
+		// given
+		Title title1 = createTitle(4L, "생존C+", "생C+");
+		Title title2 = createTitle(5L, "생존B+", "생B+");
+		Title title3 = createTitle(6L, "생존A+", "생A+");
+		titleRepository.saveAll(List.of(title1, title2, title3));
+
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("늘해랑 가입 환영", LocalDateTime.now().minusDays(20), StatType.생존력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("멋쟁이 토마토", LocalDateTime.now().minusDays(17), StatType.생존력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("오늘부터 갓생 산다.", LocalDateTime.now().minusDays(10), StatType.생존력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("내가 최고라지요", LocalDateTime.now().minusDays(7), StatType.생존력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("나야나", LocalDateTime.now().minusDays(1), StatType.생존력, 10),
+				member.getId()));
+
+		// when
+		statTitleEventHandler.checkIfGetTitleOrNot(member, StatType.생존력);
+
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(3);
+	}
+
+	@DisplayName("인싸력의 stat 포인트가 90이라면, 인싸력 관련 모든 칭호가 발급됩니다.")
+	@Test
+	void When_PopularityStatMoreThan90_Expect_GetAllOfPopularityStatTitle() {
+		// given
+		Title title1 = createTitle(7L, "인싸C+", "인C+");
+		Title title2 = createTitle(8L, "인싸B+", "인B+");
+		Title title3 = createTitle(9L, "인싸A+", "인A+");
+		titleRepository.saveAll(List.of(title1, title2, title3));
+
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("늘해랑 가입 환영", LocalDateTime.now().minusDays(20), StatType.인싸력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("멋쟁이 토마토", LocalDateTime.now().minusDays(17), StatType.인싸력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("오늘부터 갓생 산다.", LocalDateTime.now().minusDays(10), StatType.인싸력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("내가 최고라지요", LocalDateTime.now().minusDays(7), StatType.인싸력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("나야나", LocalDateTime.now().minusDays(1), StatType.인싸력, 10),
+				member.getId()));
+
+		// when
+		statTitleEventHandler.checkIfGetTitleOrNot(member, StatType.인싸력);
+
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(3);
+	}
+
+	@DisplayName("튼튼력의 stat 포인트가 90이라면, 튼튼력 관련 모든 칭호가 발급됩니다.")
+	@Test
+	void When_PowerStatMoreThan90_Expect_GetAllOfPowerStatTitle() {
+		// given
+		Title title1 = createTitle(10L, "튼튼C+", "튼C+");
+		Title title2 = createTitle(11L, "튼튼B+", "튼B+");
+		Title title3 = createTitle(12L, "튼튼A+", "튼A+");
+		titleRepository.saveAll(List.of(title1, title2, title3));
+
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("늘해랑 가입 환영", LocalDateTime.now().minusDays(20), StatType.튼튼력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("멋쟁이 토마토", LocalDateTime.now().minusDays(17), StatType.튼튼력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("오늘부터 갓생 산다.", LocalDateTime.now().minusDays(10), StatType.튼튼력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("내가 최고라지요", LocalDateTime.now().minusDays(7), StatType.튼튼력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("나야나", LocalDateTime.now().minusDays(1), StatType.튼튼력, 10),
+				member.getId()));
+
+		// when
+		statTitleEventHandler.checkIfGetTitleOrNot(member, StatType.튼튼력);
+
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(3);
+	}
+
+	@DisplayName("창의력의 stat 포인트가 90이라면, 창의력 관련 모든 칭호가 발급됩니다.")
+	@Test
+	void When_CreativeStatMoreThan90_Expect_GetAllOfCreativeStatTitle() {
+		// given
+		Title title1 = createTitle(13L, "갓생C+", "갓C+");
+		Title title2 = createTitle(14L, "갓생B+", "갓B+");
+		Title title3 = createTitle(15L, "갓생A+", "갓A+");
+		titleRepository.saveAll(List.of(title1, title2, title3));
+
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("늘해랑 가입 환영", LocalDateTime.now().minusDays(20), StatType.창의력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("멋쟁이 토마토", LocalDateTime.now().minusDays(17), StatType.창의력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("오늘부터 갓생 산다.", LocalDateTime.now().minusDays(10), StatType.창의력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("내가 최고라지요", LocalDateTime.now().minusDays(7), StatType.창의력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("나야나", LocalDateTime.now().minusDays(1), StatType.창의력, 10),
+				member.getId()));
+
+		// when
+		statTitleEventHandler.checkIfGetTitleOrNot(member, StatType.창의력);
+
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(3);
+	}
+
+	@DisplayName("최애력의 stat 포인트가 90이라면, 최애력 관련 모든 칭호가 발급됩니다.")
+	@Test
+	void When_LoveStatMoreThan90_Expect_GetAllOfLoveStatTitle() {
+		// given
+		Title title1 = createTitle(16L, "최애C+", "최C+");
+		Title title2 = createTitle(17L, "최애B+", "초ㅣB+");
+		Title title3 = createTitle(18L, "최애A+", "최ㅏA+");
+		titleRepository.saveAll(List.of(title1, title2, title3));
+
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("늘해랑 가입 환영", LocalDateTime.now().minusDays(20), StatType.최애력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("멋쟁이 토마토", LocalDateTime.now().minusDays(17), StatType.최애력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("오늘부터 갓생 산다.", LocalDateTime.now().minusDays(10), StatType.최애력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("내가 최고라지요", LocalDateTime.now().minusDays(7), StatType.최애력, 20),
+				member.getId()));
+		memberStatRepository.save(
+			StatRecord.of(StatRecordReqDto.of("나야나", LocalDateTime.now().minusDays(1), StatType.최애력, 10),
+				member.getId()));
+
+		// when
+		statTitleEventHandler.checkIfGetTitleOrNot(member, StatType.최애력);
+
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(3);
 	}
 
 	private static Title createTitle(Long id, String name, String content) {
