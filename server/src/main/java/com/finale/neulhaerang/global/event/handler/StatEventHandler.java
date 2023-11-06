012bff76cf54d515e0074d0ac3b1c7280bb8fce7
jungeunevepark <evecomcom98@naver.com>
2023-11-06 13:04:06 +0900

    Feat: Stat이 상승 했을 때, 스탯값을 확인하고 칭호를 저장하는 event 함수 구현

---
+++ server/src/main/java/com/finale/neulhaerang/global/event/handler/StatEventHandler.java
@@ -0,0 +1,105 @@
+package com.finale.neulhaerang.global.event.handler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import com.finale.neulhaerang.domain.member.document.StatRecord;
+import com.finale.neulhaerang.domain.member.entity.Member;
+import com.finale.neulhaerang.domain.member.repository.MemberStatRepository;
+import com.finale.neulhaerang.domain.routine.entity.StatType;
+import com.finale.neulhaerang.domain.title.entity.EarnedTitle;
+import com.finale.neulhaerang.domain.title.entity.Title;
+import com.finale.neulhaerang.domain.title.repository.EarnedTitleRepository;
+import com.finale.neulhaerang.domain.title.repository.TitleRepository;
+import com.finale.neulhaerang.global.event.StatEvent;
+import com.finale.neulhaerang.global.exception.title.NotExistTitleException;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class StatEventHandler {
+	private final MemberStatRepository memberStatRepository;
+	private final TitleRepository titleRepository;
+	private final EarnedTitleRepository earnedTitleRepository;
+	private Map<StatType, Long[]> titleIdByStat = new HashMap<>() {
+		{
+			put(StatType.갓생력, new Long[] {1L, 2L, 3L});
+			put(StatType.생존력, new Long[] {4L, 5L, 6L});
+			put(StatType.인싸력, new Long[] {7L, 8L, 9L});
+			put(StatType.튼튼력, new Long[] {10L, 11L, 12L});
+			put(StatType.창의력, new Long[] {13L, 14L, 15L});
+			put(StatType.최애력, new Long[] {16L, 17L, 18L});
+		}
+	};
+
+	@Async
+	@EventListener
+	public void checkLifeStat(StatEvent statEvent) {
+		checkIfGetTitleOrNot(statEvent.getMember(), StatType.갓생력);
+	}
+
+	@Async
+	@EventListener
+	public void checkSurvivalStat(StatEvent statEvent) {
+		checkIfGetTitleOrNot(statEvent.getMember(), StatType.생존력);
+	}
+
+	@Async
+	@EventListener
+	public void checkPopularityStat(StatEvent statEvent) {
+		checkIfGetTitleOrNot(statEvent.getMember(), StatType.인싸력);
+	}
+
+	@Async
+	@EventListener
+	public void checkPowerStat(StatEvent statEvent) {
+		checkIfGetTitleOrNot(statEvent.getMember(), StatType.튼튼력);
+	}
+
+	@Async
+	@EventListener
+	public void checkCreativeStat(StatEvent statEvent) {
+		checkIfGetTitleOrNot(statEvent.getMember(), StatType.창의력);
+	}
+
+	@Async
+	@EventListener
+	public void checkLoveStat(StatEvent statEvent) {
+		checkIfGetTitleOrNot(statEvent.getMember(), StatType.최애력);
+	}
+
+	private void checkIfGetTitleOrNot(Member member, StatType statType) {
+		Long[] titleId = titleIdByStat.get(statType);
+		List<StatRecord> records = memberStatRepository.findStatRecordsByStatTypeIsInAndMemberId(List.of(statType),
+			member.getId());
+
+		int weight = records.stream()
+			.map(record -> record.getWeight()).reduce(0, Integer::sum);
+
+		if (weight >= 50) {
+			earnTitle(titleId, 0, member);
+		}
+		if (weight >= 70) {
+			earnTitle(titleId, 1, member);
+		}
+		if (weight >= 90) {
+			earnTitle(titleId, 2, member);
+		}
+	}
+
+	private void earnTitle(Long[] titleId, int titleNum, Member member) {
+		Optional<Title> optionalTitle = titleRepository.findById(titleId[titleNum]);
+		if (optionalTitle.isEmpty()) {
+			throw new NotExistTitleException(member, titleId[titleNum]);
+		}
+		earnedTitleRepository.save(EarnedTitle.create(member, optionalTitle.get()));
+	}
+
+}
