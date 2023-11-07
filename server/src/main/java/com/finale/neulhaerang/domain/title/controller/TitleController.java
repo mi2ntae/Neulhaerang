ec18f55ba415ade79819eba2b59991f12d0d0549
jungeunevepark <evecomcom98@naver.com>
2023-11-07 10:40:53 +0900

    Feat: 얻은 칭호를 조회하는 API 구현

---
+++ server/src/main/java/com/finale/neulhaerang/domain/title/controller/TitleController.java
@@ -0,0 +1,27 @@
+package com.finale.neulhaerang.domain.title.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finale.neulhaerang.domain.title.dto.response.EarnedTitleResDto;
+import com.finale.neulhaerang.domain.title.service.TitleService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/title")
+@RequiredArgsConstructor
+public class TitleController {
+	private final TitleService titleService;
+
+	@GetMapping
+	public ResponseEntity<List<EarnedTitleResDto>> findEarnedTitleByMember() {
+		List<EarnedTitleResDto> earnedTitle = titleService.findEarnedTitleByMember();
+		return ResponseEntity.status(HttpStatus.OK).body(earnedTitle);
+	}
+}
