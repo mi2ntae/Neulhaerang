0bc254903aa7e053118db30c7f718222946ca1cc
jungeunevepark <evecomcom98@naver.com>
2023-11-07 10:30:29 +0900

    Feat: EarnedTitle을 Dto로 변환하는 정적 팩토리 메서드 생성

--- server/src/main/java/com/finale/neulhaerang/domain/title/dto/response/EarnedTitleResDto.java
+++ server/src/main/java/com/finale/neulhaerang/domain/title/dto/response/EarnedTitleResDto.java
@@ -1,5 +1,7 @@
 package com.finale.neulhaerang.domain.title.dto.response;
 
+import com.finale.neulhaerang.domain.title.entity.EarnedTitle;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,4 +15,12 @@ public class EarnedTitleResDto {
 	private Long titleId;
 	private String name;
 	private String content;
+
+	public static EarnedTitleResDto from(EarnedTitle earnedTitle) {
+		return EarnedTitleResDto.builder()
+			.titleId(earnedTitle.getTitle().getId())
+			.content(earnedTitle.getTitle().getContent())
+			.name(earnedTitle.getTitle().getName())
+			.build();
+	}
 }
