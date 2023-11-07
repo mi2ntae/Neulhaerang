324c32475cca794638421a3182db5ab1afaf7ba4
jungeunevepark <evecomcom98@naver.com>
2023-11-07 10:14:40 +0900

    Feat: TitleService 생성

---
+++ server/src/main/java/com/finale/neulhaerang/domain/title/service/TitleSerivceImpl.java
@@ -0,0 +1,12 @@
+package com.finale.neulhaerang.domain.title.service;
+
+import java.util.List;
+
+import com.finale.neulhaerang.domain.title.dto.response.EarnedTitleResDto;
+
+public class TitleSerivceImpl implements TitleService {
+	@Override
+	public List<EarnedTitleResDto> findEarnedTitleByMember() {
+		return null;
+	}
+}
