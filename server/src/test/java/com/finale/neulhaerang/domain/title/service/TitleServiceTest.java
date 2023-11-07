46362174f7c2f18baa6a41f93555af8481653e61
jungeunevepark <evecomcom98@naver.com>
2023-11-07 11:04:16 +0900

    Test: 칭호 장착 비지니스 로직 관련 테스트 코드 작성

--- server/src/test/java/com/finale/neulhaerang/domain/title/service/TitleServiceTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/title/service/TitleServiceTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.title.dto.response.EarnedTitleResDto;
 import com.finale.neulhaerang.domain.title.entity.EarnedTitle;
 import com.finale.neulhaerang.domain.title.entity.Title;
 import com.finale.neulhaerang.domain.title.repository.EarnedTitleRepository;
 import com.finale.neulhaerang.domain.title.repository.TitleRepository;
+import com.finale.neulhaerang.global.exception.title.NotExistTitleException;
 import com.finale.neulhaerang.global.util.BaseTest;
 
 class TitleServiceTest extends BaseTest {
@@ -45,6 +47,46 @@ class TitleServiceTest extends BaseTest {
 				tuple(19L, "슬늘생1", "슬늘1"),
 				tuple(21L, "슬늘생3", "슬늘3")
 			);
+	}
+
+	@DisplayName("장착 칭호를 변경합니다.")
+	@Test
+	void When_ModifyEquipTitle_Expect_ChangeEquipTitle() {
+		// given
+		Title title1 = createTitle(1L, "슬늘생1", "슬늘1");
+		Title title2 = createTitle(2L, "슬늘생2", "슬늘2");
+		List<Title> titles = titleRepository.saveAll(List.of(title1, title2));
+		EarnedTitle earnedTitle1 = createEarnedTitle(titles.get(0));
+		EarnedTitle earnedTitle2 = createEarnedTitle(titles.get(1));
+		earnedTitleRepository.saveAll(List.of(earnedTitle1, earnedTitle2));
+
+		// when
+		titleService.modifyEquipTitleByTitleId(titles.get(1).getId());
+
+		// then
+		Member member1 = memberRepository.findById(member.getId()).get();
+		assertThat(member1.getTitleId()).isEqualTo(titles.get(1).getId());
+	}
+
+	@DisplayName("장착 칭호 변경 시, 해당 칭호가 없다면 에러가 납니다.")
+	@Test
+	void When_ModifyEquipTitleWithNotExistTitle_Expect_NotExistTitleException() {
+		// given // when // then
+		assertThatThrownBy(() -> titleService.modifyEquipTitleByTitleId(1L))
+			.isInstanceOf(NotExistTitleException.class);
+	}
+
+	@DisplayName("장착 칭호 변경 시, 아직 얻지 못한 칭호라면 에러가 납니다.")
+	@Test
+	void When_ModifyEquipTitleWithNotEarnedTitle_Expect_ChangeEquipTitle() {
+		// given
+		Title title1 = createTitle(1L, "슬늘생1", "슬늘1");
+		Title title2 = createTitle(2L, "슬늘생2", "슬늘2");
+		List<Title> titles = titleRepository.saveAll(List.of(title1, title2));
+
+		// when // then
+		assertThatThrownBy(() -> titleService.modifyEquipTitleByTitleId(titles.get(1).getId()))
+			.isInstanceOf(NotExistTitleException.class);
 	}
 
 	private EarnedTitle createEarnedTitle(Title title) {
