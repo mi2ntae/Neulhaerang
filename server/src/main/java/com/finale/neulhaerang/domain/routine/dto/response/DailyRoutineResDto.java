d6984686585bfeb46dbd9c54752d7ad24c20ccd1
jungeunevepark <evecomcom98@naver.com>
2023-11-07 10:01:04 +0900

    Feat: 알림 여부, 알림 시간, 반복 여부도 함께 보내도록 설정

--- server/src/main/java/com/finale/neulhaerang/domain/routine/dto/response/DailyRoutineResDto.java
+++ server/src/main/java/com/finale/neulhaerang/domain/routine/dto/response/DailyRoutineResDto.java
@@ -1,34 +1,40 @@
 package com.finale.neulhaerang.domain.routine.dto.response;
 
-import com.finale.neulhaerang.domain.routine.entity.DailyRoutine;
+import java.time.LocalTime;
+import java.util.List;
 
+import com.finale.neulhaerang.domain.routine.entity.DailyRoutine;
+import com.finale.neulhaerang.domain.routine.entity.StatType;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DailyRoutineResDto {
 	private Long dailyRoutineId;
-
 	private Long routineId;
 	private String content;
 	private boolean check;
+	private boolean alarm;
+	private LocalTime alarmTime;
+	private StatType statType;
+	private List<Boolean> repeated;
 
-	@Builder
-	private DailyRoutineResDto(Long dailyRoutineId, Long routineId, String content, boolean check) {
-		this.dailyRoutineId = dailyRoutineId;
-		this.routineId = routineId;
-		this.content = content;
-		this.check = check;
-	}
-
-	public static DailyRoutineResDto from(DailyRoutine dailyRoutine) {
+	public static DailyRoutineResDto of(DailyRoutine dailyRoutine, List<Boolean> repeated) {
 		return DailyRoutineResDto.builder()
 			.dailyRoutineId(dailyRoutine.getId())
 			.routineId(dailyRoutine.getRoutine().getId())
 			.content(dailyRoutine.getRoutine().getContent())
 			.check(dailyRoutine.isCheck())
+			.alarm(dailyRoutine.getRoutine().isAlarm())
+			.alarmTime(dailyRoutine.getRoutine().getAlarmTime())
+			.statType(dailyRoutine.getRoutine().getStatType())
+			.repeated(repeated)
 			.build();
 	}
 }
--- server/src/main/java/com/finale/neulhaerang/domain/routine/dto/response/RoutineResDto.java
+++ server/src/main/java/com/finale/neulhaerang/domain/routine/dto/response/RoutineResDto.java
@@ -1,28 +1,36 @@
 package com.finale.neulhaerang.domain.routine.dto.response;
 
-import com.finale.neulhaerang.domain.routine.entity.Routine;
+import java.time.LocalTime;
+import java.util.List;
 
+import com.finale.neulhaerang.domain.routine.entity.Routine;
+import com.finale.neulhaerang.domain.routine.entity.StatType;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoutineResDto {
 	private Long routineId;
-
 	private String content;
+	private StatType statType;
+	private boolean alarm;
+	private LocalTime alarmTime;
+	private List<Boolean> repeated;
 
-	@Builder
-	private RoutineResDto(Long routineId, String content) {
-		this.routineId = routineId;
-		this.content = content;
-	}
-
-	public static RoutineResDto from(Routine routine) {
+	public static RoutineResDto of(Routine routine, List<Boolean> repeated) {
 		return RoutineResDto.builder()
 			.routineId(routine.getId())
 			.content(routine.getContent())
+			.alarm(routine.isAlarm())
+			.alarmTime(routine.getAlarmTime())
+			.statType(routine.getStatType())
+			.repeated(repeated)
 			.build();
 	}
 }
