c427a6488e3dd6fa02d2e9c070d7ad539db2cb22
jungeunevepark <evecomcom98@naver.com>
2023-11-07 14:40:44 +0900

    Feat: 칭호 이벤트를 다루는 최상위 class 생성

---
+++ server/src/main/java/com/finale/neulhaerang/global/event/handler/TitleEventHandler.java
@@ -0,0 +1,47 @@
+package com.finale.neulhaerang.global.event.handler;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.finale.neulhaerang.domain.member.entity.Member;
+import com.finale.neulhaerang.domain.member.repository.MemberStatRepository;
+import com.finale.neulhaerang.domain.title.entity.EarnedTitle;
+import com.finale.neulhaerang.domain.title.entity.Title;
+import com.finale.neulhaerang.domain.title.repository.EarnedTitleRepository;
+import com.finale.neulhaerang.domain.title.repository.TitleRepository;
+import com.finale.neulhaerang.global.exception.title.NotExistTitleException;
+import com.finale.neulhaerang.global.notification.dto.request.TitleNotificationReqDto;
+import com.finale.neulhaerang.global.notification.service.NotificationService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@Transactional
+public class TitleEventHandler {
+	@Autowired
+	protected MemberStatRepository memberStatRepository;
+	@Autowired
+	protected TitleRepository titleRepository;
+	@Autowired
+	protected EarnedTitleRepository earnedTitleRepository;
+	@Autowired
+	protected NotificationService notificationService;
+
+	protected void getTitle(Long titleId, Member member) {
+		Optional<Title> optionalTitle = titleRepository.findById(titleId);
+		if (optionalTitle.isEmpty()) {
+			throw new NotExistTitleException(member, titleId);
+		}
+		if (!earnedTitleRepository.existsByTitle_IdAndMember(titleId, member)) {
+			earnedTitleRepository.save(EarnedTitle.create(member, optionalTitle.get()));
+			notificationService.sendNotificationByToken(member.getId(),
+				TitleNotificationReqDto.create(member, optionalTitle.get()));
+		} else {
+			log.info(member.getNickname() + "님이 이미 획득한 칭호(title_id=" + titleId + ")이기 때문에 발급하지 않습니다.");
+		}
+	}
+}
