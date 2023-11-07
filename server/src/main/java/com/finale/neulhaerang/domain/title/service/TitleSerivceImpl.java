f9760af75eb53a44271828f34e6cbc88f206127a
jungeunevepark <evecomcom98@naver.com>
2023-11-07 11:11:14 +0900

    Feat: 장착 칭호를 변경하는 비지니스 로직 구현

--- server/src/main/java/com/finale/neulhaerang/domain/title/service/TitleSerivceImpl.java
+++ server/src/main/java/com/finale/neulhaerang/domain/title/service/TitleSerivceImpl.java
@@ -1,14 +1,21 @@
 package com.finale.neulhaerang.domain.title.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.finale.neulhaerang.domain.member.entity.Member;
+import com.finale.neulhaerang.domain.member.repository.MemberRepository;
 import com.finale.neulhaerang.domain.title.dto.response.EarnedTitleResDto;
 import com.finale.neulhaerang.domain.title.entity.EarnedTitle;
+import com.finale.neulhaerang.domain.title.entity.Title;
 import com.finale.neulhaerang.domain.title.repository.EarnedTitleRepository;
+import com.finale.neulhaerang.domain.title.repository.TitleRepository;
+import com.finale.neulhaerang.global.exception.title.NotEarnedTitleException;
+import com.finale.neulhaerang.global.exception.title.NotExistTitleException;
 import com.finale.neulhaerang.global.util.AuthenticationHandler;
 
 import lombok.RequiredArgsConstructor;
@@ -19,6 +26,8 @@ import lombok.RequiredArgsConstructor;
 public class TitleSerivceImpl implements TitleService {
 	private final AuthenticationHandler authenticationHandler;
 	private final EarnedTitleRepository earnedTitleRepository;
+	private final TitleRepository titleRepository;
+	private final MemberRepository memberRepository;
 
 	@Override
 	public List<EarnedTitleResDto> findEarnedTitleByMember() {
@@ -31,6 +40,14 @@ public class TitleSerivceImpl implements TitleService {
 
 	@Override
 	public void modifyEquipTitleByTitleId(Long titleId) {
-
+		Member member = memberRepository.findById(authenticationHandler.getLoginMemberId()).get();
+		Optional<Title> title = titleRepository.findById(titleId);
+		if (title.isEmpty()) {
+			throw new NotExistTitleException(member, titleId);
+		}
+		if (!earnedTitleRepository.existsByTitle_IdAndMember(titleId, member)) {
+			throw new NotEarnedTitleException(member, titleId);
+		}
+		member.updateTitleId(titleId);
 	}
 }
