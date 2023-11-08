128b9ebf3526559e16d18ff8d8c04a39e625fb5e
jungeunevepark <evecomcom98@naver.com>
2023-11-08 14:48:43 +0900

    Test: 현재 날짜와 입력 받은 시간 사이에 알림을 받는 모든 todo를 불러오는 JPA 쿼리 테스트 코드 작성

--- server/src/test/java/com/finale/neulhaerang/domain/todo/repository/TodoRepositoryTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/todo/repository/TodoRepositoryTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.finale.neulhaerang.domain.routine.entity.StatType;
-import com.finale.neulhaerang.global.util.BaseTest;
 import com.finale.neulhaerang.domain.todo.entity.Todo;
+import com.finale.neulhaerang.global.util.BaseTest;
 
 class TodoRepositoryTest extends BaseTest {
 	@Autowired
@@ -21,28 +21,22 @@ class TodoRepositoryTest extends BaseTest {
 
 	@Test
 	@DisplayName("로그인 한 사용자가 요청한 날짜에 등록된 모든 Todo 불러오는 JPA 쿼리 테스트")
-	public void When_FindTodoByDate_Expect_AllTodoList(){
+	public void When_FindTodoByDate_Expect_AllTodoList() {
 		// given
-		Todo todo1 = this.createTodo("todo1",StatType.생존력,LocalDateTime.now(),false);
-		Todo todo2 = this.createTodo("todo2",StatType.인싸력,LocalDateTime.now(),false);
-		Todo todo3 = this.createTodo("todo3",StatType.갓생력,LocalDateTime.now(),false);
-		Todo todo4 = this.createTodo("todo4",StatType.창의력,LocalDateTime.of(2023,10,30,13,30),false);
-		Todo todo5 = Todo.builder()
-			.member(member)
-			.todoDate(LocalDateTime.now())
-			.content("todo5")
-			.statType(StatType.최애력)
-			.status(true)
-			.build();
+		Todo todo1 = this.createTodo("todo1", StatType.생존력, LocalDateTime.now(), false, true, false);
+		Todo todo2 = this.createTodo("todo2", StatType.인싸력, LocalDateTime.now(), false, true, false);
+		Todo todo3 = this.createTodo("todo3", StatType.갓생력, LocalDateTime.now(), false, true, false);
+		Todo todo4 = this.createTodo("todo4", StatType.창의력, LocalDateTime.of(2023, 10, 30, 13, 30), false, true, false);
+		Todo todo5 = this.createTodo("todo5", StatType.최애력, LocalDateTime.now(), false, true, true);
 		todoRepository.saveAll(List.of(todo1, todo2, todo3, todo4, todo5));
 
 		// when
 		List<Todo> todoList = todoRepository.findTodosByMemberAndStatusIsFalseAndTodoDateIsBetween(
-			member,LocalDate.now().atStartOfDay(),LocalDate.now().atTime(LocalTime.MAX));
+			member, LocalDate.now().atStartOfDay(), LocalDate.now().atTime(LocalTime.MAX));
 
 		// then
 		assertThat(todoList).hasSize(3);
-		for(Todo todo:todoList){
+		for (Todo todo : todoList) {
 			assertThat(todo.getTodoDate().toLocalDate()).isEqualTo(LocalDate.now());
 			assertThat(todo.isStatus()).isEqualTo(false);
 		}
@@ -50,38 +44,65 @@ class TodoRepositoryTest extends BaseTest {
 
 	@Test
 	@DisplayName("로그인 한 사용자가 요청한 날짜에 완료한 Todo 리스트 불러오는 JPA 쿼리 테스트")
-	public void When_FindTodoByDate_Expect_DoneTodoList(){
+	public void When_FindTodoByDate_Expect_DoneTodoList() {
 		// given
-		Todo todo1 = this.createTodo("todo1",StatType.생존력,LocalDateTime.now(),false); // 완료하지 않은 투두
-		Todo todo2 = this.createTodo("todo2",StatType.인싸력,LocalDateTime.now(),true); // 완료한 투두
-		Todo todo3 = this.createTodo("todo3",StatType.갓생력,LocalDateTime.now(),true); // 완료한 투두
-		Todo todo4 = this.createTodo("todo4",StatType.창의력,LocalDateTime.of(2023,10,30,13,30),true);
+		Todo todo1 = this.createTodo("todo1", StatType.생존력, LocalDateTime.now(), false, true, false); // 완료하지 않은 투두
+		Todo todo2 = this.createTodo("todo2", StatType.인싸력, LocalDateTime.now(), true, true, false); // 완료한 투두
+		Todo todo3 = this.createTodo("todo3", StatType.갓생력, LocalDateTime.now(), true, false, false); // 완료한 투두
 		// 완료했지만 오늘이 아닌 날의 투두
-		Todo todo5 = Todo.builder() // 삭제한 투두
-			.member(member)
-			.todoDate(LocalDateTime.now())
-			.content("todo5")
-			.statType(StatType.최애력)
-			.status(true)
-			.build();
+		Todo todo4 = this.createTodo("todo4", StatType.창의력, LocalDateTime.of(2023, 10, 30, 13, 30), true, false, false);
+		// 삭제한 투두
+		Todo todo5 = this.createTodo("todo5", StatType.최애력, LocalDateTime.now(), false, true, true);
 		todoRepository.saveAll(List.of(todo1, todo2, todo3, todo4, todo5));
 
 		// when
 		List<Todo> todoList = todoRepository.findTodosByMemberAndStatusIsFalseAndCheckIsTrueAndTodoDateIsBetween(
-			member,LocalDate.now().atStartOfDay(),LocalDate.now().atTime(LocalTime.MAX));
+			member, LocalDate.now().atStartOfDay(), LocalDate.now().atTime(LocalTime.MAX));
 
 		// then
 		assertThat(todoList).hasSize(2);
-		for(Todo todo:todoList){
+		for (Todo todo : todoList) {
 			assertThat(todo.getTodoDate().toLocalDate()).isEqualTo(LocalDate.now());
 			assertThat(todo.isStatus()).isEqualTo(false);
 			assertThat(todo.isCheck()).isEqualTo(true);
 		}
 	}
 
-	private Todo createTodo(String content, StatType statType, LocalDateTime todoDate, boolean check){
+	@Test
+	@DisplayName("현재 시간에 알람을 받기로 되어있는 모든 todo를 불러오는 JPA 쿼리 테스트")
+	public void When_FindTodoByDateAndAlarmIsTrue_Expect_AlarmTodoList() {
+		// given
+		LocalDate now = LocalDate.now();
+		LocalTime time = LocalTime.now();
+		Todo todo1 = this.createTodo("todo1", StatType.생존력, LocalDateTime.of(now, time.withSecond(0)), false, true,
+			false); // 완료하지 않은 투두
+		Todo todo2 = this.createTodo("todo2", StatType.인싸력, LocalDateTime.of(now, time.withSecond(59)), true, true,
+			false); // 완료한 투두
+		Todo todo3 = this.createTodo("todo3", StatType.갓생력, LocalDateTime.of(now, time), true, false, false); // 완료한 투두
+		Todo todo4 = this.createTodo("todo4", StatType.창의력, LocalDateTime.of(2023, 10, 30, 13, 30), true, true, false);
+		// 삭제한 투두
+		Todo todo5 = this.createTodo("todo5", StatType.최애력, LocalDateTime.of(now, time), false, true, true);
+		todoRepository.saveAll(List.of(todo1, todo2, todo3, todo4, todo5));
+
+		// when
+		List<Todo> todoList = todoRepository.findTodosByStatusIsFalseAndTodoDateIsBetweenAndAlarmIsTrue(
+			LocalDateTime.of(now, time.withSecond(0)), LocalDateTime.of(now, time.plusMinutes(1).withSecond(0)));
+
+		// then
+		assertThat(todoList).hasSize(2)
+			.extracting("content", "alarm")
+			.containsExactlyInAnyOrder(
+				tuple("todo1", true),
+				tuple("todo2", true)
+			);
+	}
+
+	private Todo createTodo(String content, StatType statType, LocalDateTime todoDate, boolean check, boolean alarm,
+		boolean status) {
 		return Todo.builder()
 			.member(member)
+			.alarm(alarm)
+			.status(status)
 			.todoDate(todoDate)
 			.content(content)
 			.statType(statType)
