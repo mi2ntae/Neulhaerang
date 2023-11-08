9d7da2319f3d1944146b0fa274939864999932e3
jungeunevepark <evecomcom98@naver.com>
2023-11-08 10:03:50 +0900

    Test: weather title을 발급받는 로직에 대해 테스트 코드 작성

---
+++ server/src/test/java/com/finale/neulhaerang/global/event/handler/WeatherTitleEventHandlerTest.java
@@ -0,0 +1,154 @@
+package com.finale.neulhaerang.global.event.handler;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.finale.neulhaerang.domain.routine.entity.DailyRoutine;
+import com.finale.neulhaerang.domain.routine.entity.Routine;
+import com.finale.neulhaerang.domain.routine.entity.StatType;
+import com.finale.neulhaerang.domain.routine.repository.DailyRoutineRepository;
+import com.finale.neulhaerang.domain.routine.repository.RoutineRepository;
+import com.finale.neulhaerang.domain.title.entity.EarnedTitle;
+import com.finale.neulhaerang.domain.title.entity.Title;
+import com.finale.neulhaerang.domain.title.repository.EarnedTitleRepository;
+import com.finale.neulhaerang.domain.title.repository.TitleRepository;
+import com.finale.neulhaerang.domain.todo.entity.Todo;
+import com.finale.neulhaerang.domain.todo.repository.TodoRepository;
+import com.finale.neulhaerang.global.util.BaseTest;
+
+class WeatherTitleEventHandlerTest extends BaseTest {
+	@Autowired
+	private DailyRoutineRepository dailyRoutineRepository;
+	@Autowired
+	private TodoRepository todoRepository;
+	@Autowired
+	private TitleRepository titleRepository;
+	@Autowired
+	private EarnedTitleRepository earnedTitleRepository;
+	@Autowired
+	private WeatherTitleEventHandler weatherTitleEventHandler;
+	@Autowired
+	private RoutineRepository routineRepository;
+
+	@DisplayName("10일 연속 routine를 70퍼센트 이상 달성했다면 칭호를 1개 받습니다.")
+	@Test
+	void When_SunnyContinuous10Days_Expect_GetSunnyTitle() {
+		// given
+		member.updateCreateDate(LocalDateTime.now().minusDays(100));
+		Title title1 = createTitle(25L, "해10", "태양10");
+		Title title2 = createTitle(26L, "해50", "태양50");
+		titleRepository.saveAll(List.of(title1, title2));
+		Routine routine = Routine.builder()
+			.content("hi")
+			.member(member)
+			.alarm(false)
+			.statType(StatType.갓생력)
+			.repeated("0000000")
+			.build();
+		Routine save = routineRepository.save(routine);
+		for (int i = 1; i <= 10; i++) {
+			createDailyRoutine(i, save);
+		}
+
+		// when
+		weatherTitleEventHandler.checkIfGetWeatherTitle(member);
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(1);
+	}
+
+	@DisplayName("9일 연속 todo를 70퍼센트 이상 달성했다면 칭호를 0개 받습니다.")
+	@Test
+	void When_SunnyContinuous9Days_Expect_GetSunnyTitle() {
+		// given
+		member.updateCreateDate(LocalDateTime.now().minusDays(100));
+		Title title1 = createTitle(25L, "해10", "태양10");
+		Title title2 = createTitle(26L, "해50", "태양50");
+		titleRepository.saveAll(List.of(title1, title2));
+		for (int i = 1; i <= 9; i++) {
+			createCheckTodo(i);
+		}
+
+		// when
+		weatherTitleEventHandler.checkIfGetWeatherTitle(member);
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(0);
+	}
+
+	@DisplayName("49일 연속 todo를 70퍼센트 이상 달성했다면 칭호를 1개 받습니다.")
+	@Test
+	void When_SunnyContinuous49Days_Expect_GetSunnyTitle() {
+		// given
+		member.updateCreateDate(LocalDateTime.now().minusDays(100));
+		Title title1 = createTitle(25L, "해10", "태양10");
+		Title title2 = createTitle(26L, "해50", "태양50");
+		titleRepository.saveAll(List.of(title1, title2));
+		for (int i = 1; i <= 49; i++) {
+			createCheckTodo(i);
+		}
+
+		// when
+		weatherTitleEventHandler.checkIfGetWeatherTitle(member);
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(1);
+	}
+
+	@DisplayName("50일 연속 todo를 70퍼센트 이상 달성했다면 칭호를 2개 받습니다.")
+	@Test
+	void When_SunnyContinuous50Days_Expect_GetSunnyTitle() {
+		// given
+		member.updateCreateDate(LocalDateTime.now().minusDays(100));
+		Title title1 = createTitle(25L, "해10", "태양10");
+		Title title2 = createTitle(26L, "해50", "태양50");
+		titleRepository.saveAll(List.of(title1, title2));
+		for (int i = 1; i <= 50; i++) {
+			createCheckTodo(i);
+		}
+
+		// when
+		weatherTitleEventHandler.checkIfGetWeatherTitle(member);
+		// then
+		List<EarnedTitle> earnedTitles = earnedTitleRepository.findAll();
+		assertThat(earnedTitles).hasSize(2);
+	}
+
+	private Title createTitle(Long id, String name, String content) {
+		return Title.builder()
+			.id(id)
+			.name(name)
+			.content(content)
+			.build();
+	}
+
+	public void createCheckTodo(int i) {
+		Todo todo = Todo.builder()
+			.member(member)
+			.check(true)
+			.todoDate(LocalDateTime.now().minusDays(i))
+			.content("hi")
+			.alarm(false)
+			.status(false)
+			.statType(StatType.갓생력)
+			.build();
+		todoRepository.save(todo);
+	}
+
+	public void createDailyRoutine(int i, Routine routine) {
+		DailyRoutine dailyRoutine = DailyRoutine.builder()
+			.routineDate(LocalDate.now().minusDays(i))
+			.routine(routine)
+			.check(true)
+			.status(false)
+			.build();
+		dailyRoutineRepository.save(dailyRoutine);
+	}
+}
