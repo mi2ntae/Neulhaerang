0cd49268e5aa6efaf41c8a1072cf867127cb25bc
임성원 <dnjsndnjs@gmail.com>
2023-11-08 15:42:56 +0900

    Feat: 안드로이드 체크리스트 삭제 다이얼로그 안내 텍스트 수정

--- android/ui/src/main/java/com/finale/neulhaerang/ui/app/checklist/CheckListModifyScreen.kt
+++ android/ui/src/main/java/com/finale/neulhaerang/ui/app/checklist/CheckListModifyScreen.kt
@@ -1,7 +1,9 @@
 package com.finale.neulhaerang.ui.app.checklist
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -33,6 +35,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -241,11 +244,14 @@ fun CheckListDeleteDialog(
         },
         text = {
             if (viewModel.routine) {
-                Column {
-                    Text(text = "이 루틴을 더 이상 반복하지 않으시겠습니까?")
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
                     Checkbox(
                         checked = viewModel.stopRoutine,
                         onCheckedChange = { viewModel.setStopRoutine(it) })
+                    Text(text = "앞으로 반복하지 않기")
                 }
             }
         }
