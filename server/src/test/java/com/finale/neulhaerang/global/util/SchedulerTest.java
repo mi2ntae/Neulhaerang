3f5b1ae5fcca655f0f88f03f2d98107c6b4ff944
rladbwls30 <rladbwls30@naver.com>
2023-11-08 09:42:04 +0900

    Test: 테스트 코드 수정

--- server/src/test/java/com/finale/neulhaerang/global/util/SchedulerTest.java
+++ server/src/test/java/com/finale/neulhaerang/global/util/SchedulerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import com.finale.neulhaerang.domain.letter.entity.Letter;
 import com.finale.neulhaerang.domain.letter.repository.LetterRepository;
 import com.finale.neulhaerang.domain.member.document.StatRecord;
+import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.member.repository.MemberStatRepository;
 import com.finale.neulhaerang.domain.routine.entity.DailyRoutine;
 import com.finale.neulhaerang.domain.routine.entity.Routine;
@@ -78,8 +80,11 @@ class SchedulerTest extends BaseTest {
 		LocalDate date = LocalDate.now().minusDays(1);
 		createTodoAndRoutine(date);
 
+		List<Member> memberList = new ArrayList<>();
+		memberList.add(member);
+
 		// when
-		scheduler.modifyStat(member, date);
+		scheduler.modifyStat(memberList, date);
 
 		// then
 		List<StatRecord> records = memberStatRepository.findAll();
@@ -98,26 +103,29 @@ class SchedulerTest extends BaseTest {
 
 	@Test
 	@DisplayName("스케줄러가 실행되면 전 날 완료한 투두와 루틴 리스트로 편지를 작성해 줍니다.")
-	void When_Scheduler_Expect_GenerateLetterByTodoAndRoutine(){
+	void When_Scheduler_Expect_GenerateLetterByTodoAndRoutine() {
 		// given
 		LocalDate date = LocalDate.now().minusDays(1);
 		createTodoAndRoutine(date);
 
+		List<Member> memberList = new ArrayList<>();
+		memberList.add(member);
+
 		// when
-		scheduler.createLetter(date);
+		scheduler.createLetter(memberList, date);
 
 		// then
 		List<Letter> letter = letterRepository.findAll();
 		assertThat(letter).hasSize(1);
 	}
 
-	private void createTodoAndRoutine(LocalDate date){
-		Todo todo1 = this.createTodo("7시 미라클 모닝",StatType.생존력,date.atTime(13,30),true);
-		Todo todo2 = this.createTodo("친구랑 곱창 먹기",StatType.인싸력,date.atTime(13,30),true);
-		Todo todo3 = this.createTodo("헬스장 가기",StatType.갓생력,date.atTime(13,30),true);
-		Todo todo4 = this.createTodo("코딩 문제 풀기",StatType.창의력,date.atTime(13,30),true);
-		Todo todo5 = this.createTodo("단지 이뻐해주기", StatType.최애력,date.atTime(13,30),true);
-		Todo todo6 = this.createTodo("연예인 사진 찾아보기", StatType.최애력,date.atTime(13,30),false);
+	private void createTodoAndRoutine(LocalDate date) {
+		Todo todo1 = this.createTodo("7시 미라클 모닝", StatType.생존력, date.atTime(13, 30), true);
+		Todo todo2 = this.createTodo("친구랑 곱창 먹기", StatType.인싸력, date.atTime(13, 30), true);
+		Todo todo3 = this.createTodo("헬스장 가기", StatType.갓생력, date.atTime(13, 30), true);
+		Todo todo4 = this.createTodo("코딩 문제 풀기", StatType.창의력, date.atTime(13, 30), true);
+		Todo todo5 = this.createTodo("단지 이뻐해주기", StatType.최애력, date.atTime(13, 30), true);
+		Todo todo6 = this.createTodo("연예인 사진 찾아보기", StatType.최애력, date.atTime(13, 30), false);
 		todoRepository.saveAll(List.of(todo1, todo2, todo3, todo4, todo5, todo6));
 
 		Routine routine1 = createRoutine("애견카페가기", "0010000", false, StatType.최애력);
@@ -131,7 +139,8 @@ class SchedulerTest extends BaseTest {
 		DailyRoutine dailyRoutine4 = createDailyRoutine(routine2, true, date);
 		DailyRoutine dailyRoutine5 = createDailyRoutine(routine3, true, date);
 		DailyRoutine dailyRoutine6 = createDailyRoutine(routine3, false, date);
-		dailyRoutineRepository.saveAll(List.of(dailyRoutine1, dailyRoutine2, dailyRoutine3, dailyRoutine4, dailyRoutine5, dailyRoutine6));
+		dailyRoutineRepository.saveAll(
+			List.of(dailyRoutine1, dailyRoutine2, dailyRoutine3, dailyRoutine4, dailyRoutine5, dailyRoutine6));
 	}
 
 	private Routine createRoutine(String content, String repeated, boolean alarm, StatType statType) {
