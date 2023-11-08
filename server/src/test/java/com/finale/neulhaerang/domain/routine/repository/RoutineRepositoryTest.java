a6d731ca6c4956e8b55d64da118a46e53e0a5bd6
jungeunevepark <evecomcom98@naver.com>
2023-11-08 13:40:59 +0900

    Test: 삭제되지 않은 루틴 중, 오늘 실행되며 현재 시간에 알림이 예정된 모든 루틴을 가져오는 함수 테스트 코드 작성

--- server/src/test/java/com/finale/neulhaerang/domain/routine/repository/RoutineRepositoryTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/routine/repository/RoutineRepositoryTest.java
@@ -3,6 +3,7 @@ package com.finale.neulhaerang.domain.routine.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
@@ -45,6 +46,32 @@ class RoutineRepositoryTest extends BaseTest {
 			);
 	}
 
+	@DisplayName("해당 날짜 요일에 실행되는 루틴 중 현재 시간에 알림 설정이 되어있는 것들을 가져옵니다. 이때 삭제 예정 날짜가 해당 날짜 이후여야 합니다.")
+	@Test
+	void When_GetDateAndDayOfValueAndAlarmTime_Expect_GetListOfRoutine() {
+		// given
+		LocalTime now = LocalTime.now();
+		Routine routine1 = createRoutine("양치하기1", "0010000", false, StatType.생존력, null);
+		Routine routine2 = createRoutine("양치하기2", "0110000", true, StatType.갓생력, LocalDate.of(2023, 8, 20), now);
+		Routine routine3 = createRoutine("양치하기3", "0111000", true, StatType.최애력, LocalDate.of(2023, 8, 19), now);
+		Routine routine4 = createRoutine("양치하기4", "0101000", false, StatType.인싸력, null);
+
+		routineRepository.saveAll(List.of(routine1, routine2, routine3, routine4));
+
+		// when
+		String dayOfValue = "__1____";
+		LocalDate date = LocalDate.of(2023, 8, 19);
+		List<Routine> routines = routineRepository.findRoutinesByDayOfValueAndAlarmIsTrueAndAlarmTimeIsNow(dayOfValue,
+			date, now);
+
+		// then
+		Assertions.assertThat(routines).hasSize(1)
+			.extracting("content", "repeated", "statType", "alarm", "alarmTime")
+			.containsExactlyInAnyOrder(
+				tuple("양치하기2", "0110000", StatType.갓생력, true, now)
+			);
+	}
+
 	private Routine createRoutine(String content, String repeated, boolean alarm,
 		StatType statType, LocalDate deleteDate) {
 		return Routine.builder()
@@ -52,6 +79,19 @@ class RoutineRepositoryTest extends BaseTest {
 			.content(content)
 			.repeated(repeated)
 			.alarm(alarm)
+			.statType(statType)
+			.deleteDate(deleteDate)
+			.build();
+	}
+
+	private Routine createRoutine(String content, String repeated, boolean alarm,
+		StatType statType, LocalDate deleteDate, LocalTime now) {
+		return Routine.builder()
+			.member(member)
+			.content(content)
+			.repeated(repeated)
+			.alarm(alarm)
+			.alarmTime(now)
 			.statType(statType)
 			.deleteDate(deleteDate)
 			.build();
