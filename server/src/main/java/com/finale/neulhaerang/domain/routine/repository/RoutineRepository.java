014b11bbdda8dff137ebc3ac2b3a3ae5eaf88ec1
jungeunevepark <evecomcom98@naver.com>
2023-11-08 13:40:46 +0900

    Feat: 삭제되지 않은 루틴 중, 오늘 실행되며 현재 시간에 알림이 예정된 모든 루틴을 가져오는 함수 구현

--- server/src/main/java/com/finale/neulhaerang/domain/routine/repository/RoutineRepository.java
+++ server/src/main/java/com/finale/neulhaerang/domain/routine/repository/RoutineRepository.java
@@ -1,6 +1,7 @@
 package com.finale.neulhaerang.domain.routine.repository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +16,14 @@ public interface RoutineRepository extends JpaRepository<Routine, Long> {
 		+ "where r.repeated like :dayOfValue "
 		+ "and (r.delete_date > :date or r.delete_date is null)", nativeQuery = true)
 	List<Routine> findRoutinesByDayOfValue(String dayOfValue, LocalDate date);
+
+	@Query(value = "select * "
+		+ "from routine r "
+		+ "where r.repeated like :dayOfValue "
+		+ "and (r.delete_date > :date or r.delete_date is null) "
+		+ "and alarm is true "
+		+ "and alarm_time= :now", nativeQuery = true)
+	List<Routine> findRoutinesByDayOfValueAndAlarmIsTrueAndAlarmTimeIsNow(String dayOfValue, LocalDate date,
+		LocalTime now);
+
 }
