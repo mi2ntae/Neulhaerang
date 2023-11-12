29d9cdf8c3ba5436fcc810d2d27952bc98cad134
kosigi19 <kosigi19@naver.com>
2023-11-13 00:32:47 +0900

    Feat: getCharacterItems, modifyCharacterItems, defeatLazyMonster API 추가

--- android/data/src/main/java/com/finale/neulhaerang/data/api/MemberApi.kt
+++ android/data/src/main/java/com/finale/neulhaerang/data/api/MemberApi.kt
@@ -1,9 +1,14 @@
 package com.finale.neulhaerang.data.api
 
+import com.finale.neulhaerang.data.model.request.MemberItemReqDto
+import com.finale.neulhaerang.data.model.request.TodoReqDto
+import com.finale.neulhaerang.data.model.response.MemberItemResDto
 import com.finale.neulhaerang.data.model.response.MemberStatResDto
 import com.finale.neulhaerang.data.model.response.MemberStatusResDto
 import com.finale.neulhaerang.data.util.ResponseResult
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -17,6 +22,19 @@ interface MemberApi {
 
     @GET("$BASE/stat/{memberId}")
     suspend fun getMemberStat(@Path("memberId") memberId: Long): ResponseResult<List<MemberStatResDto>>
+
+    @GET("$BASE/character/{memberId}")
+    suspend fun getCharacterItems(@Path("memberId") memberId: Long): ResponseResult<MemberItemResDto>
+
+    @PATCH("$BASE/character")
+    suspend fun modifyCharacterItems(
+        @Body request: MemberItemReqDto,
+    ): ResponseResult<Any>
+
+    @POST("/ar/indolence")
+    suspend fun defeatLazyMonster(
+        @Body request: MemberItemReqDto,
+    ): ResponseResult<Any>
 
     companion object {
         private const val BASE = "member"
