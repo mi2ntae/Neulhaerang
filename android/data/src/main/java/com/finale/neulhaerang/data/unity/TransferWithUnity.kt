68daacaa49aa390be6580868f088cd44a5cf00cb
kosigi19 <kosigi19@naver.com>
2023-11-13 00:17:48 +0900

    Feat: 사용자 능력치 조회 메서드 추가

--- android/data/src/main/java/com/finale/neulhaerang/data/unity/TransferWithUnity.kt
+++ android/data/src/main/java/com/finale/neulhaerang/data/unity/TransferWithUnity.kt
@@ -1,25 +1,36 @@
 package com.finale.neulhaerang.data.unity
 
 import android.util.Log
+import com.finale.neulhaerang.data.DataStoreApplication
 import com.finale.neulhaerang.data.api.AuthApi
+import com.finale.neulhaerang.data.api.MemberApi
 import com.finale.neulhaerang.data.entity.UserData
+import com.finale.neulhaerang.data.model.response.MemberStatResDto
+import com.finale.neulhaerang.data.model.response.MemberStatusResDto
 import com.finale.neulhaerang.data.util.onSuccess
 import com.google.gson.Gson
 import com.unity3d.player.UnityPlayer
 import com.unity3d.player.UnityPlayerActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-
+@OptIn(DelicateCoroutinesApi::class)
 class TransferWithUnity {
+    // 데이터를 받을 유니티 스크립트가 컴포넌트로 붙어 있는 게임오브젝트명
+    private val unityGameObject = "AndroidController"
     // 메소드가 static (companion object) 이면 안 됨
     fun receiveMessage(userId: String) {
         runBlocking {
             GlobalScope.launch {
-                AuthApi.instance.postCheck().onSuccess {
-                    Log.i("heejeong", "api test")
-                    Log.i("heejeong", it.data.toString())
-                }
+                val memberId =
+                    DataStoreApplication.getInstance().getDataStore().getMemberId().firstOrNull() ?: 0
+                MemberApi.instance.getMemberStat(memberId)
+                    .onSuccess { (_, data) ->
+                        checkNotNull(data)
+                        Log.i("heejeong", data.toString())
+                    }
             }.join()
         }
         Log.i("heejeong", "TransferWithUnity $userId")
@@ -41,6 +52,31 @@ class TransferWithUnity {
         // 데이터를 받을 유니티 스크립트의 메소드명
         val unityMethod = "ReceiveMessage"
 
+        UnityPlayer.UnitySendMessage(unityGameObject, unityMethod, jsonMessage)
+    }
+
+    /*
+    * 유저 능력치 조회
+    * */
+    fun getMemberStats() {
+        runBlocking {
+            GlobalScope.launch {
+                val memberId =
+                    DataStoreApplication.getInstance().getDataStore().getMemberId().firstOrNull() ?: 0
+                MemberApi.instance.getMemberStat(memberId)
+                    .onSuccess { (_, data) ->
+                        checkNotNull(data)
+                        Log.i("heejeong", data.toString())
+                        sendMemberStats(data)
+                    }
+            }.join()
+        }
+    }
+
+    private fun sendMemberStats(memberStats: List<MemberStatResDto>) {
+        val gson = Gson()
+        val jsonMessage = gson.toJson(memberStats)
+        val unityMethod = "ReceiveMessage"
         UnityPlayer.UnitySendMessage(unityGameObject, unityMethod, jsonMessage)
     }
 }
