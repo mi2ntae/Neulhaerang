b75759c983af2ad6a9590893eb555c320f1de260
kjy6135@naver.com <kjy6135@naver.com>
2023-11-12 13:09:46 +0900

    Feat: 의상 장착 활성화 버튼 스크립트 구현

--- unity-source/Assets/Scripts/GlassesPanel.cs
+++ unity-source/Assets/Scripts/GlassesPanel.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GlassesPanel : MonoBehaviour
 {
     public List<ClothesButton> glassesButtons;
     public List<GameObject> glassesObjects;
 
+    public List<Sprite> glassesOff;
+    public List<Sprite> glassesOn;
+
     public void ClickTab(int id)
     {
         // active -> not active
-        if (glassesObjects[id].activeSelf) { glassesObjects[id].SetActive(false); }
+        if (glassesObjects[id].activeSelf)
+        {
+            glassesObjects[id].SetActive(false);
+            glassesButtons[id].GetComponent<Image>().sprite = glassesOff[id];
+        }
         else // not active -> all off -> activate
         {
             for (int i = 0; i < glassesObjects.Count; i++)
@@ -18,10 +26,12 @@ public class GlassesPanel : MonoBehaviour
                 if (i == id)
                 {
                     glassesObjects[i].SetActive(true);
+                    glassesButtons[i].GetComponent<Image>().sprite = glassesOn[i];
                 }
                 else
                 {
                     glassesObjects[i].SetActive(false);
+                    glassesButtons[i].GetComponent<Image>().sprite = glassesOff[i];
                 }
             }
         }
--- unity-source/Assets/Scripts/ScarfPanel.cs
+++ unity-source/Assets/Scripts/ScarfPanel.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScarfPanel : MonoBehaviour
 {
     public List<ClothesButton> scarfButtons;
     public List<GameObject> scarfObjects;
 
+    public List<Sprite> scarfOff;
+    public List<Sprite> scarfOn;
+
     public void ClickTab(int id)
     {
         // active -> not active
-        if (scarfObjects[id].activeSelf) { scarfObjects[id].SetActive(false); }
+        if (scarfObjects[id].activeSelf)
+        {
+            scarfObjects[id].SetActive(false);
+            scarfButtons[id].GetComponent<Image>().sprite = scarfOff[id];
+        }
         else // not active -> all off -> activate
         {
             for (int i = 0; i < scarfObjects.Count; i++)
@@ -18,10 +26,12 @@ public class ScarfPanel : MonoBehaviour
                 if (i == id)
                 {
                     scarfObjects[i].SetActive(true);
+                    scarfButtons[i].GetComponent<Image>().sprite = scarfOn[i];
                 }
                 else
                 {
                     scarfObjects[i].SetActive(false);
+                    scarfButtons[i].GetComponent<Image>().sprite = scarfOff[i];
                 }
             }
         }
