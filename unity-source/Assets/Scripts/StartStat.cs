d5f88376164e2f46e97c08e80e095b6e37a8fe6a
kjy6135@naver.com <kjy6135@naver.com>
2023-11-12 22:29:57 +0900

    Feat: Radar chart 로직 구현

---
+++ unity-source/Assets/Scripts/StartStat.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartStat : MonoBehaviour
+{
+    [SerializeField] private StatsRadarChart statsRadarChart;
+    private void Start()
+    {
+
+        int[] scores = new int[6];
+
+        scores = getScore(scores);
+
+        /*
+         * A+ => 2500
+         * A  => 2000
+         * B+ => 1500
+         * B  => 1000
+         * C+ => 500
+         */        
+
+        Stats stats = new Stats(scores[0], scores[1], scores[2], scores[3], scores[4], scores[5]);
+        statsRadarChart.SetStats(stats);
+    }
+
+    private int[] getScore(int[] scores)
+    {
+        // TODO
+        // get score from server
+
+        // godsang
+        scores[0] = 2500;
+        // survive
+        scores[1] = 2000;
+        // inssa
+        scores[2] = 1500;
+        // teunteun
+        scores[3] = 2000;
+        // goodidea
+        scores[4] = 2500;
+        // love
+        scores[5] = 1500;
+
+        return scores;
+    }
+}
