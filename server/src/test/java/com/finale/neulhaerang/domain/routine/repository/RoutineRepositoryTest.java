b250b1d23265853c0c0670e4a466d8c00c6ef3a3
jungeunevepark <evecomcom98@naver.com>
2023-11-13 13:24:11 +0900

    Test: 버그 수정에 따라 테스트 코드 변경

--- server/src/test/java/com/finale/neulhaerang/domain/routine/repository/RoutineRepositoryTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/routine/repository/RoutineRepositoryTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.finale.neulhaerang.domain.member.entity.Member;
 import com.finale.neulhaerang.domain.routine.entity.Routine;
 import com.finale.neulhaerang.domain.routine.entity.StatType;
 import com.finale.neulhaerang.global.util.BaseTest;
@@ -36,6 +37,71 @@ class RoutineRepositoryTest extends BaseTest {
 		String dayOfValue = "__1____";
 		LocalDate date = LocalDate.of(2023, 8, 19);
 		List<Routine> routines = routineRepository.findRoutinesByDayOfValue(dayOfValue, date);
+
+		// then
+		Assertions.assertThat(routines).hasSize(2)
+			.extracting("content", "repeated", "statType")
+			.containsExactlyInAnyOrder(
+				tuple("양치하기1", "0010000", StatType.생존력),
+				tuple("양치하기2", "0110000", StatType.갓생력)
+			);
+	}
+
+	@DisplayName("해당 날짜 요일에 실행되는 루틴들을 가져옵니다. 이때 본인의 루틴이 아니어도 가져옵니다.")
+	@Test
+	void When_GetDateAndDayOfValue_Expect_AllOfRoutine() {
+		// given
+
+		Routine routine1 = createRoutine("양치하기1", "0010000", false, StatType.생존력, null);
+		Routine routine2 = createRoutine("양치하기2", "0110000", false, StatType.갓생력, LocalDate.of(2023, 8, 20));
+		Member otherMember = memberRepository.save(Member.builder().kakaoId(1).nickname("test").build());
+		Routine routine3 = Routine.builder()
+			.member(otherMember)
+			.content("test")
+			.repeated("1111111")
+			.alarm(false)
+			.statType(StatType.갓생력)
+			.build();
+
+		routineRepository.saveAll(List.of(routine1, routine2, routine3));
+
+		// when
+		String dayOfValue = "__1____";
+		LocalDate date = LocalDate.of(2023, 8, 19);
+		List<Routine> routines = routineRepository.findRoutinesByDayOfValue(dayOfValue, date);
+
+		// then
+		Assertions.assertThat(routines).hasSize(3)
+			.extracting("content", "repeated", "statType")
+			.containsExactlyInAnyOrder(
+				tuple("양치하기1", "0010000", StatType.생존력),
+				tuple("양치하기2", "0110000", StatType.갓생력),
+				tuple("test", "1111111", StatType.갓생력)
+			);
+	}
+
+	@DisplayName("해당 날짜 요일에 실행되는 루틴들을 가져옵니다. 이때 본인의 루틴만 가져옵니다.")
+	@Test
+	void When_GetDateAndDayOfValue_Expect_OnlyMyRoutine() {
+		// given
+
+		Routine routine1 = createRoutine("양치하기1", "0010000", false, StatType.생존력, null);
+		Routine routine2 = createRoutine("양치하기2", "0110000", false, StatType.갓생력, LocalDate.of(2023, 8, 20));
+		Member otherMember = memberRepository.save(Member.builder().kakaoId(1).nickname("test").build());
+		Routine routine3 = Routine.builder()
+			.member(otherMember)
+			.content("test")
+			.repeated("1111111")
+			.alarm(false)
+			.statType(StatType.갓생력)
+			.build();
+
+		routineRepository.saveAll(List.of(routine1, routine2, routine3));
+
+		// when
+		String dayOfValue = "__1____";
+		LocalDate date = LocalDate.of(2023, 8, 19);
+		List<Routine> routines = routineRepository.findRoutinesByDayOfValueAndMember(dayOfValue, date, member.getId());
 
 		// then
 		Assertions.assertThat(routines).hasSize(2)
