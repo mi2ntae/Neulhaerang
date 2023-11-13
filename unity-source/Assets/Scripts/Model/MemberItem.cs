e376cf743f988de99b6b1d8b24bf38e2d42553d3
kosigi19 <kosigi19@naver.com>
2023-11-13 15:36:15 +0900

    Feat: title필드 추가, 생성자 추가, toString 추가

--- unity-source/Assets/Scripts/Model/MemberItem.cs
+++ unity-source/Assets/Scripts/Model/MemberItem.cs
@@ -12,9 +12,26 @@ public class MemberItem
     private int hat;
     [SerializeField]
     private int scarf;
+    [SerializeField]
+    private int title;
 
     public int Backpack { get => backpack; }
     public int Glasses { get => glasses; }
     public int Hat { get => hat; }
     public int Scarf { get => scarf; }
+    public int Title { get => title; }
+
+    public MemberItem(int backpack, int glasses, int hat, int scarf, int title)
+    {
+        this.backpack = backpack;
+        this.glasses = glasses;
+        this.hat = hat;
+        this.scarf = scarf;
+        this.title = title;
+    }
+
+    public override string ToString()
+    {
+        return "[Backpack]" + Backpack + " [Glasses]" + Glasses + " [Hat]" + Hat + " [Scarf]" + Scarf + " [Title]" + Title;
+    }
 }
