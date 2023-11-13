ce26138d75cc66de6deed0def12f837d70fdf991
김민태 <kimmintae981019@gmail.com>
2023-11-13 16:02:38 +0900

    Fix: UI 패키지 manifest 수정, 권한 처리 방침 코틀린으로 변경

---
+++ android/app/src/main/java/com/finale/neulhaerang/PermissionsRationaleActivity.kt
@@ -0,0 +1,16 @@
+package com.finale.neulhaerang
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class PermissionsRationaleActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
