6457c80b33d2315848c6a1d06d95c38b204a7f4c
kosigi19 <kosigi19@naver.com>
2023-11-13 13:45:14 +0900

    Feat: Unity Response Body 모델 추가

---
+++ unity-source/Assets/Scripts/Model/MemberTitles.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class MemberTitle
+{
+    [SerializeField]
+    private long titleId;
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private string content;
+
+    public long TitleId { get => titleId; }
+    public string Name { get => name; }
+    public string Content { get => content; }
+}
