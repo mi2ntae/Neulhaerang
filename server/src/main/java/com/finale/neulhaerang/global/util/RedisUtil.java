196c1ea47151496244d4416cf9db22b6b0cb1892
김민태 <kimmintae981019@gmail.com>
2023-11-13 14:16:42 +0900

    Feat: Redis Geospatioal 이용한 로그인 멤버 좌표값 저장 및 주변 100m 이내 사용자 찾기 구현

--- server/src/main/java/com/finale/neulhaerang/global/util/RedisUtil.java
+++ server/src/main/java/com/finale/neulhaerang/global/util/RedisUtil.java
@@ -1,7 +1,14 @@
 package com.finale.neulhaerang.global.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Metric;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +20,32 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RedisUtil {
 	private final RedisTemplate<String, String> redisTemplate;
+	private final String geoKey = "members";
 
-	public void setData(String key, String value, Long expiredTime){
-		redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
+	public void setData(String deviceToken, String refreshKey, Long expiredTime){
+		redisTemplate.opsForValue().set(deviceToken, refreshKey, expiredTime, TimeUnit.MILLISECONDS);
+	}
+
+	public List<String> changeGeo(Point point, String deviceId) {
+		redisTemplate.opsForGeo().add(geoKey, point, deviceId);
+		Metric metric = RedisGeoCommands.DistanceUnit.METERS;
+		GeoResults<RedisGeoCommands.GeoLocation<String>> members = redisTemplate.opsForGeo().radius(geoKey, deviceId, new Distance(100, metric));
+		List<String> deviceIds = new ArrayList<>();
+		members.forEach(geoLocationGeoResult -> {
+			RedisGeoCommands.GeoLocation<String> content = geoLocationGeoResult.getContent();
+			String device = content.getName();
+			deviceIds.add(device);
+			log.debug("Around Device Id : "+device);
+		});
+		return deviceIds;
 	}
 
 	public String getData(String key){
 		return redisTemplate.opsForValue().get(key);
 	}
 
-	public void deleteData(String key){
-		redisTemplate.delete(key);
+	public void deleteData(String deviceToken){
+		redisTemplate.delete(deviceToken);
+		redisTemplate.opsForGeo().remove(geoKey, deviceToken);
 	}
 }
