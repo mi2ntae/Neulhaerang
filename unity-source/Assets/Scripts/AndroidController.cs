34f55080fe0efb54cd45aa53555328fa3a776dcb
kjy6135@naver.com <kjy6135@naver.com>
2023-11-14 07:06:09 +0900

    Feat: 스텟, 상태, 몬스터잡기, 아이템, 아이템갱신 코드 작성 완료

--- unity-source/Assets/Scripts/AndroidController.cs
+++ unity-source/Assets/Scripts/AndroidController.cs
@@ -1,9 +1,39 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AndroidController : MonoBehaviour
 {
     AndroidJavaObject _pluginInstance;
+
+    [SerializeField] private StatsRadarChart statsRadarChart;
+
+    // Monster button
+    public Button monsterButton;
+
+    // Mypage Status
+    public List<GameObject> bagList;
+    public List<GameObject> glassesList;
+    public List<GameObject> minihatList;
+    public List<GameObject> scarfList;
+    public Button titleObject;
+
+    // Clothes Button Image List
+    public List<Sprite> bagOn;
+    public List<Sprite> glassesOn;
+    public List<Sprite> minihatOn;
+    public List<Sprite> scarfOn;
+    public List<Sprite> titleSprites;
+
+    public List<ClothesButton> bagButtons;
+    public List<ClothesButton> glassesButtons;
+    public List<ClothesButton> minihatButtons;
+    public List<ClothesButton> scarfButtons;
+    public List<TitleButton> titleButtons;
+
+    
+    
 
     void Awake()
     {
@@ -17,12 +47,12 @@ public class AndroidController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //RequestMemberStats();
-        //RequestMemberStatus();
-        //RequestDefeatMonster();
-        //RequestCharacterItems();
-        //ModifyCharacterItems(new MemberItem(1,2,1,1,1));
-        RequestUserTitles();
+        RequestMemberStats();
+        RequestMemberStatus();
+        RequestDefeatMonster();
+        RequestCharacterItems();
+        //ModifyCharacterItems(new MemberItem(1, 2, 1, 1, 1));
+        //RequestUserTitles();
     }
 
     // Update is called once per frame
@@ -46,6 +76,47 @@ public class AndroidController : MonoBehaviour
         Debug.Log("heejeong [ReceiveMemberStats]" + jsonMessage);
         MemberStat datas = JsonUtility.FromJson<MemberStat>(jsonMessage);
         Debug.Log("heejeong Life" + datas.ToString());
+
+        // StartStat
+
+        int[] scores = new int[6];
+
+        scores[0] = datas.Life;
+        scores[1] = datas.Survival;
+        scores[2] = datas.Popularity;
+        scores[3] = datas.Power;
+        scores[4] = datas.Creative;
+        scores[5] = datas.Love;
+
+        scores = changeValue(scores);
+        /*
+         * A+ => 2500
+         * A  => 2000
+         * B+ => 1500
+         * B  => 1000
+         * C+ => 500
+         */
+
+        Stats stats = new Stats(scores[0], scores[1], scores[2], scores[3], scores[4], scores[5]);
+        statsRadarChart.SetStats(stats);
+    }
+
+    /*
+     * Radar chart 에 맞게 수치 조정
+     */
+    private int[] changeValue(int[] scores)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (scores[i] >= 150) scores[i] = 2500;
+            else
+            {
+                scores[i] = scores[i] * 2500 / 150;
+            }
+
+            Debug.Log("change score value - index : " + i + "  value : " + scores[i]);
+        }
+        return scores;
     }
 
     /**
@@ -63,13 +134,23 @@ public class AndroidController : MonoBehaviour
 
         Debug.Log("heejeong 나태도" + datas.Indolence);
         Debug.Log("heejeong 피로도" + datas.Tiredness);
+
+        // 나태도가 50 이하면 나태괴물 안나타나게
+        if (monsterButton != null && datas.Indolence < 50)
+        {
+            Image buttonImage = monsterButton.GetComponent<Image>();
+            Color newColor = buttonImage.color;
+            newColor.a = 0.0f;
+            buttonImage.color = newColor;
+        }
+
     }
 
 
     /**
      * 나태 괴물 처치 완료
      */
-    void RequestDefeatMonster()
+    public void RequestDefeatMonster()
     {
         string androidMethod = "defeatLazyMonster";
         _pluginInstance.Call(androidMethod);
@@ -89,12 +170,66 @@ public class AndroidController : MonoBehaviour
         Debug.Log("heejeong [ReceiveCharacterItems]" + jsonMessage);
         MemberItem datas = JsonUtility.FromJson<MemberItem>(jsonMessage);
         Debug.Log("heejeong 착장한 사용자 아이템 ::" + datas.ToString());
+
+        /*
+         * MemberItem list
+         * 
+         * backpack;
+         * glasses;
+         * hat;
+         * scarf;
+         * title;
+         */
+
+        // 아이템을 장착했다면 Active
+
+        if (datas.Backpack != 0)
+        {
+            bagList[datas.Backpack].SetActive(true);
+            bagButtons[datas.Backpack].GetComponent<Image>().sprite = bagOn[datas.Backpack];
+        }
+
+        if (datas.Glasses != 0)
+        {
+            glassesList[datas.Glasses].SetActive(true);
+            glassesButtons[datas.Glasses].GetComponent<Image>().sprite = glassesOn[datas.Glasses];
+        }
+
+        if (datas.Hat != 0)
+        {
+            minihatList[datas.Hat].SetActive(true);
+            minihatButtons[datas.Hat].GetComponent<Image>().sprite = minihatOn[datas.Hat];
+        }
+
+        if (datas.Scarf != 0)
+        {
+            scarfList[datas.Scarf].SetActive(true);
+            scarfButtons[datas.Scarf].GetComponent<Image>().sprite = scarfOn[datas.Scarf];
+        }
+
+        if (datas.Title != 0)
+        {
+            // transparent
+            Image buttonImage = titleObject.GetComponent<Image>();
+            Color newColor = buttonImage.color;
+            newColor.a = 1.0f;
+            buttonImage.color = newColor;
+            Debug.Log("color : " + newColor);
+            titleObject.GetComponent<Image>().sprite = titleSprites[datas.Title];
+        }
+
+        // 아이템 전역 저장
+        PlayerPrefs.SetInt("Bag", datas.Backpack);
+        PlayerPrefs.SetInt("Glasses", datas.Glasses);
+        PlayerPrefs.SetInt("Minihat", datas.Hat);
+        PlayerPrefs.SetInt("Scarf", datas.Scarf);
+        PlayerPrefs.SetInt("Title", datas.Title);
     }
 
     /**
      * 유저 장착 아이템 수정
      */
-    void ModifyCharacterItems(MemberItem item)
+    public void ModifyCharacterItems(MemberItem item)
     {
         string datas = JsonUtility.ToJson(item);
         Debug.Log("heejeong 유저 아이템 수정 목록::" + item.ToString());
@@ -122,5 +257,7 @@ public class AndroidController : MonoBehaviour
             Debug.Log("heejeong 유저 보유 칭호 목록::" + lt.TitleId);
             Debug.Log("heejeong 유저 보유 칭호 목록::" + lt.Content);
         }
+
+        /*TODO*/
     }
 }
