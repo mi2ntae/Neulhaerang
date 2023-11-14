2e80262fb05e6126c73d02f9c20e3679536ae4a5
jungeunevepark <evecomcom98@naver.com>
2023-11-14 13:30:09 +0900

    Test: 멤버 unit test 추가

--- server/src/test/java/com/finale/neulhaerang/domain/member/entity/MemberTest.java
+++ server/src/test/java/com/finale/neulhaerang/domain/member/entity/MemberTest.java
@@ -1,7 +1,9 @@
 package com.finale.neulhaerang.domain.member.entity;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.assertj.core.api.SoftAssertions.*;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,6 +34,51 @@ class MemberTest {
 			s.assertThat(member.getKakaoId()).isEqualTo(kakaoId);
 			s.assertThat(member.getNickname()).isEqualTo(nickname);
 		});
+	}
+
+	@DisplayName("멤버가 생성됩니다.")
+	@Test
+	void When_CreateMember_Expect_IsOk() {
+		// given
+		long kakaoId = 111111;
+		String nickname = "김민태";
+
+		// when
+		Member member = Member.create(kakaoId, nickname);
+
+		// then
+		assertSoftly(s -> {
+			s.assertThat(member.getKakaoId()).isEqualTo(kakaoId);
+			s.assertThat(member.getNickname()).isEqualTo(nickname);
+		});
+	}
+
+	@DisplayName("멤버의 탈퇴 날짜를 변경합니다.")
+	@Test
+	void When_ModifyWithdrawalDate_Expect_Update() {
+		// given
+		Member member = Member.create(1234565L, "늘해랑");
+		LocalDateTime now = LocalDateTime.now();
+
+		// when
+		member.updateWithdrawalDate(now);
+
+		// then
+		assertThat(member.getWithdrawalDate()).isEqualTo(now);
+	}
+
+	@DisplayName("멤버의 생성 날짜를 변경합니다. For test")
+	@Test
+	void When_ModifyCreateDate_Expect_Update() {
+		// given
+		Member member = Member.create(1234565L, "늘해랑");
+		LocalDateTime now = LocalDateTime.now();
+
+		// when
+		member.updateCreateDate(now);
+
+		// then
+		assertThat(member.getCreateDate()).isEqualTo(now);
 	}
 
 	private Member createMember(long kakaoId, String nickname) {
