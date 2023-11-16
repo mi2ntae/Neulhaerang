2eb9d0c5842c735e8ff5bddb57f5d0884fb2339b
kosigi19 <kosigi19@naver.com>
2023-11-16 17:30:24 +0900

    Fix: ReceiveNearByUsers에서 사용자 조회하도록 변경

--- unity-source/Assets/Scripts/Social/AroundPeopleManager.cs
+++ unity-source/Assets/Scripts/Social/AroundPeopleManager.cs
@@ -43,84 +43,6 @@ public class AroundPeopleManager : MonoBehaviour
         }
 
         RequestNearByUsers();
-        int size = data.members.Length;
-
-        Debug.Log("heejeong "+size);
-
-        for(int i = 0; i < size; i++)
-        {
-            Transform transform = Character[i].transform.Find("Root_M/Spine1_M/Chest_M");
-            GameObject Back = transform.Find("BagA").gameObject;
-            GameObject Neck = transform.Find("Neck_M/ScarfA").gameObject;
-            GameObject Face = transform.Find("Neck_M/Head_M/GlassesA").gameObject;
-            GameObject Head = transform.Find("Neck_M/Head_M/MinihatA").gameObject;
-
-            int index = i;
-
-            if (data.members[i].Title != 0)
-            {
-                // 에디터에서 프로젝트 경로에 있는 이미지의 경로
-                Sprite sprite = Resources.Load<Sprite>("TitleImage/on/on_"+data.members[i].Title.ToString()+".png");
-
-                if (sprite != null)
-                {
-                    TitleImage[index].sprite = sprite;
-                }
-                else
-                {
-                    Debug.LogError("Failed to load image");
-                }
-            }
-
-            Debug.Log("heejeong Title Success");
-
-            if (data.members[i].Scarf != 0)
-            {
-                GameObject instantiatedNeck = Instantiate(Scarfs[data.members[i].Scarf-1], transform.Find("Neck_M"));
-                instantiatedNeck.transform.position = Neck.transform.position;
-                instantiatedNeck.transform.rotation = Neck.transform.rotation;
-                instantiatedNeck.SetActive(true);
-            }
-
-            Debug.Log("heejeong Scarf Success");
-
-            if (data.members[i].Backpack != 0)
-            {
-                GameObject instantiatedBack = Instantiate(Bags[data.members[i].Backpack-1], transform);
-                instantiatedBack.transform.position = Back.transform.position;
-                instantiatedBack.transform.rotation = Back.transform.rotation;
-                instantiatedBack.SetActive(true);
-            }
-
-            Debug.Log("heejeong Backpack Success");
-
-            if (data.members[i].Glasses != 0)
-            {
-                GameObject instantiatedFace = Instantiate(Bags[data.members[i].Glasses - 1], transform.Find("Neck_M/Head_M"));
-                instantiatedFace.transform.position = Face.transform.position;
-                instantiatedFace.transform.rotation = Face.transform.rotation;
-                instantiatedFace.SetActive(true);
-            }
-
-            Debug.Log("heejeong Glasses Success");
-
-            if (data.members[i].Hat != 0)
-            {
-                GameObject instantiatedHead = Instantiate(Bags[data.members[i].Hat - 1], transform.Find("Neck_M/Head_M"));
-                instantiatedHead.transform.position = Head.transform.position;
-                instantiatedHead.transform.rotation = Head.transform.rotation;
-                instantiatedHead.SetActive(true);
-            }
-
-            Debug.Log("heejeong Hat Success");
-
-            Destroy(Back);
-            Destroy(Neck);
-            Destroy(Face);
-            Destroy(Head);
-
-            Character[i].SetActive(true);
-        }
     }
 
     /**
@@ -166,6 +88,84 @@ public class AroundPeopleManager : MonoBehaviour
         foreach (AroundMember it in data.members)
         {
             Debug.Log("heejeong 근처 사용자::" + it.ToString());
+        }
+        int size = data.members.Length;
+
+        Debug.Log("heejeong " + size);
+
+        for (int i = 0; i < size; i++)
+        {
+            Transform transform = Character[i].transform.Find("Root_M/Spine1_M/Chest_M");
+            GameObject Back = transform.Find("BagA").gameObject;
+            GameObject Neck = transform.Find("Neck_M/ScarfA").gameObject;
+            GameObject Face = transform.Find("Neck_M/Head_M/GlassesA").gameObject;
+            GameObject Head = transform.Find("Neck_M/Head_M/MinihatA").gameObject;
+
+            int index = i;
+
+            if (data.members[i].Title != 0)
+            {
+                // 에디터에서 프로젝트 경로에 있는 이미지의 경로
+                Sprite sprite = Resources.Load<Sprite>("TitleImage/on/on_" + data.members[i].Title.ToString() + ".png");
+
+                if (sprite != null)
+                {
+                    TitleImage[index].sprite = sprite;
+                }
+                else
+                {
+                    Debug.LogError("Failed to load image");
+                }
+            }
+
+            Debug.Log("heejeong Title Success");
+
+            if (data.members[i].Scarf != 0)
+            {
+                GameObject instantiatedNeck = Instantiate(Scarfs[data.members[i].Scarf - 1], transform.Find("Neck_M"));
+                instantiatedNeck.transform.position = Neck.transform.position;
+                instantiatedNeck.transform.rotation = Neck.transform.rotation;
+                instantiatedNeck.SetActive(true);
+            }
+
+            Debug.Log("heejeong Scarf Success");
+
+            if (data.members[i].Backpack != 0)
+            {
+                GameObject instantiatedBack = Instantiate(Bags[data.members[i].Backpack - 1], transform);
+                instantiatedBack.transform.position = Back.transform.position;
+                instantiatedBack.transform.rotation = Back.transform.rotation;
+                instantiatedBack.SetActive(true);
+            }
+
+            Debug.Log("heejeong Backpack Success");
+
+            if (data.members[i].Glasses != 0)
+            {
+                GameObject instantiatedFace = Instantiate(Bags[data.members[i].Glasses - 1], transform.Find("Neck_M/Head_M"));
+                instantiatedFace.transform.position = Face.transform.position;
+                instantiatedFace.transform.rotation = Face.transform.rotation;
+                instantiatedFace.SetActive(true);
+            }
+
+            Debug.Log("heejeong Glasses Success");
+
+            if (data.members[i].Hat != 0)
+            {
+                GameObject instantiatedHead = Instantiate(Bags[data.members[i].Hat - 1], transform.Find("Neck_M/Head_M"));
+                instantiatedHead.transform.position = Head.transform.position;
+                instantiatedHead.transform.rotation = Head.transform.rotation;
+                instantiatedHead.SetActive(true);
+            }
+
+            Debug.Log("heejeong Hat Success");
+
+            Destroy(Back);
+            Destroy(Neck);
+            Destroy(Face);
+            Destroy(Head);
+
+            Character[i].SetActive(true);
         }
     }
 
