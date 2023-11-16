faaa8dc2cc2f68ec2f108a6fbdba18525e7c7208
임성원 <dnjsndnjs@gmail.com>
2023-11-16 16:45:57 +0900

    Refactor: 안드로이드 메인페이지 데이터 통신 비동기 동시에 실행

--- android/domain/src/main/java/com/finale/neulhaerang/domain/MainScreenViewModel.kt
+++ android/domain/src/main/java/com/finale/neulhaerang/domain/MainScreenViewModel.kt
@@ -24,8 +24,12 @@ import com.finale.neulhaerang.data.model.response.TodoResDto
 import com.finale.neulhaerang.data.util.ResponseResult
 import com.finale.neulhaerang.data.util.onFailure
 import com.finale.neulhaerang.data.util.onSuccess
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -50,6 +54,8 @@ class MainScreenViewModel : ViewModel() {
     private val _routineList = mutableStateListOf<Routine>()
     private val _todoList = mutableStateListOf<Todo>()
     private val _letterText = mutableStateOf("")
+
+    private val _loading = mutableStateOf(false)
 
     init {
         viewModelScope.launch {
@@ -76,6 +82,8 @@ class MainScreenViewModel : ViewModel() {
         get() = _todoList
     val letterText: String
         get() = _letterText.value
+    val loading: Boolean
+        get() = _loading.value
 
     val yearMonth: String
         get() = DateTimeFormatter.ofPattern("yyyy-MM").format(selectedDate)
@@ -129,26 +137,42 @@ class MainScreenViewModel : ViewModel() {
 
     private fun setDataFromDateTime() {
         viewModelScope.launch {
+            _loading.value = true
             // 완료 퍼센트 가져오기
             if (beforeYearMonth != yearMonth) {
                 updateTodoDoneList()
             }
             // 오늘의 체크리스트
-            RoutineApi.instance.getRoutine(selectedDate)
-                .onSuccess { initRoutine(it.data!!) }
-                .onFailure {
-                    Log.w(TAG, "setDataFromDateTime: fail ${it.code} ${it.message}")
+            val deferred = listOf(
+                async {
+                    withContext(Dispatchers.IO) {
+                        RoutineApi.instance.getRoutine(selectedDate)
+                            .onSuccess { initRoutine(it.data!!) }
+                            .onFailure {
+                                Log.w(TAG, "setDataFromDateTime: fail ${it.code} ${it.message}")
+                            }
+                    }
+                },
+                async {
+                    withContext(Dispatchers.IO) {
+                        TodoApi.instance.getTodo(selectedDate)
+                            .onSuccess { initTodo(it.data!!) }
+                            .onFailure {
+                                Log.w(TAG, "setDataFromDateTime: fail ${it.code} ${it.message}")
+                            }
+                    }
+                },
+                // 오늘의 편지
+                async {
+                    withContext(Dispatchers.IO) {
+                        LetterApi.instance.getLetter(selectedDate).onSuccess {
+                            setLetterText(it.data ?: "")
+                        }
+                    }
                 }
-            TodoApi.instance.getTodo(selectedDate)
-                .onSuccess { initTodo(it.data!!) }
-                .onFailure {
-                    Log.w(TAG, "setDataFromDateTime: fail ${it.code} ${it.message}")
-                }
-            // 오늘의 편지
-            LetterApi.instance.getLetter(selectedDate).onSuccess {
-                setLetterText(it.data ?: "")
-//                setLetterText("안녕, 싸피야!\n" + "\n" + "오늘은 내가 한 일들을 알려줄게. 아침에는 8시에 일어났어. 새로운 하루가 시작되는 건 항상 기대돼.\n" + "\n" + "오전에는 SSAFY 수업을 들었어. 오늘은 좀 어려운 프로그래밍 과제를 풀어봤어. 물론 동료들과 함께 협력하면서 문제를 해결했지. \uD83D\uDCAA\n" + "\n" + "점심 때는 친구들을 만나서 곱창전골을 먹었어. 맛있게 먹고 좋은 대화를 나누는 건 항상 즐거운 시간이야.\n" + "\n" + "양치하고 머리 감기도 잊지 않았어. 건강하게 지내려고 노력하고 있어. \uD83E\uDDB7 \uD83D\uDC87\n" + "\n" + "마지막으로, 1시에 잠자리에 들었어. 하루 동안 열심히 노력했지만, 늦게 자는 건 힘들어. \uD83D\uDE34\n" + "\n" + "앞으로도 같이 좋은 순간을 만들고 함께 성장해보자. 싸피야, 내일은 더 좋은 날이 오기를 기대해! \uD83D\uDE0A\uD83C\uDF1F")
-            }
+            )
+            deferred.awaitAll()
+            _loading.value = false
         }
     }
 
