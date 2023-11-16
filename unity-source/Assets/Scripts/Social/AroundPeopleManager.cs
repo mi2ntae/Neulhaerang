c1cc1e9fd13c678d07123295d37afa0e1f321478
rladbwls30 <rladbwls30@naver.com>
2023-11-16 16:46:58 +0900

    Feat: 안드로이드랑 연결

--- unity-source/Assets/Scripts/Social/AroundPeopleManager.cs
+++ unity-source/Assets/Scripts/Social/AroundPeopleManager.cs
@@ -17,6 +17,23 @@ public class AroundPeopleManager : MonoBehaviour
 
     public AroundMembers data;
 
+    public static AroundPeopleManager instance;
+
+    AndroidJavaObject _pluginInstance;
+
+
+    void Awake()
+    {
+        // 데이터를 받을 안드로이드 플러그인의 클래스 주소
+        string androidClass = "com.finale.neulhaerang.data.unity.SocialWithUnity";
+
+        // 데이터를 받을 안드로이드 클래스를 인스턴스화
+        _pluginInstance = new AndroidJavaObject(androidClass);
+
+        // 싱글톤
+        instance = this;
+    }
+
     void Start()
     {
         for(int i = 0; i < 3; i++)
@@ -25,10 +42,10 @@ public class AroundPeopleManager : MonoBehaviour
             CharaterInfo[i].SetActive(false);
         }
 
-        //AndroidController.instance.RequestNearByUsers();
+        RequestNearByUsers();
+        int size = data.members.Length;
 
-        //int size = data.members.Length;
-        int size = 1;/////////////////////////////////////////////수정
+        Debug.Log("heejeong "+size);
 
         for(int i = 0; i < size; i++)
         {
@@ -40,60 +57,62 @@ public class AroundPeopleManager : MonoBehaviour
 
             int index = i;
 
-            //if (data.members[i].Title != 0)
-            //{
-            // 에디터에서 프로젝트 경로에 있는 이미지의 경로
-            //Sprite sprite = Resources.Load<Sprite>("TitleImage/on/on_"+data.members[i].Title.toString()+".png");
-            Sprite sprite = Resources.Load<Sprite>("TitleImage/on/on_4");
-
-            if (sprite != null)
+            if (data.members[i].Title != 0)
             {
-                TitleImage[index].sprite = sprite;
-            }
-            else
-            {
-                Debug.LogError("Failed to load image");
-            }
-            //}
+                // 에디터에서 프로젝트 경로에 있는 이미지의 경로
+                Sprite sprite = Resources.Load<Sprite>("TitleImage/on/on_"+data.members[i].Title.ToString()+".png");
 
-            //if (data.members[i].Scarf != 0)
-            //{
-            // Instantiate a new Hat Prefab
-            //GameObject instantiatedNeck = Instantiate(Scarfs[data.members[i].Neck-1], transform.Find("Neck_M"));
-                GameObject instantiatedNeck = Instantiate(Scarfs[2], transform.Find("Neck_M"));
+                if (sprite != null)
+                {
+                    TitleImage[index].sprite = sprite;
+                }
+                else
+                {
+                    Debug.LogError("Failed to load image");
+                }
+            }
+
+            Debug.Log("heejeong Title Success");
+
+            if (data.members[i].Scarf != 0)
+            {
+                GameObject instantiatedNeck = Instantiate(Scarfs[data.members[i].Scarf-1], transform.Find("Neck_M"));
                 instantiatedNeck.transform.position = Neck.transform.position;
                 instantiatedNeck.transform.rotation = Neck.transform.rotation;
-                instantiatedNeck.name="ScarfA";
                 instantiatedNeck.SetActive(true);
-            //}
+            }
 
-            //if (data.members[i].Backpack != 0)
-            //{
-            //GameObject instantiatedNeck = Instantiate(Bags[data.members[i].Backpack-1], transform.Find("Neck_M"));
-            GameObject instantiatedBack = Instantiate(Bags[7], transform);
-            instantiatedBack.transform.position = Back.transform.position;
-            instantiatedBack.transform.rotation = Back.transform.rotation;
-            instantiatedBack.name = "BagA";
-            instantiatedBack.SetActive(true);
-            //}
-            //if (data.members[i].Glasses != 0)
-            //{
-            //GameObject instantiatedNeck = Instantiate(Bags[data.members[i].Glasses-1], transform.Find("Neck_M"))
-            GameObject instantiatedFace = Instantiate(Glasses[1], transform.Find("Neck_M/Head_M"));
-            instantiatedFace.transform.position = Face.transform.position;
-            instantiatedFace.transform.rotation = Face.transform.rotation;
-            instantiatedFace.name = "GlassesA";
-            instantiatedFace.SetActive(true);
-            //}
-            //if (data.members[i].Hat != 0)
-            //{
-            //GameObject instantiatedNeck = Instantiate(Bags[data.members[i].Hat-1], transform.Find("Neck_M"))
-            GameObject instantiatedHead = Instantiate(Hats[2], transform.Find("Neck_M/Head_M"));
-            instantiatedHead.transform.position = Head.transform.position;
-            instantiatedHead.transform.rotation = Head.transform.rotation;
-            instantiatedHead.name = "HatA";
-            instantiatedHead.SetActive(true);
-            //}
+            Debug.Log("heejeong Scarf Success");
+
+            if (data.members[i].Backpack != 0)
+            {
+                GameObject instantiatedBack = Instantiate(Bags[data.members[i].Backpack-1], transform);
+                instantiatedBack.transform.position = Back.transform.position;
+                instantiatedBack.transform.rotation = Back.transform.rotation;
+                instantiatedBack.SetActive(true);
+            }
+
+            Debug.Log("heejeong Backpack Success");
+
+            if (data.members[i].Glasses != 0)
+            {
+                GameObject instantiatedFace = Instantiate(Bags[data.members[i].Glasses - 1], transform.Find("Neck_M/Head_M"));
+                instantiatedFace.transform.position = Face.transform.position;
+                instantiatedFace.transform.rotation = Face.transform.rotation;
+                instantiatedFace.SetActive(true);
+            }
+
+            Debug.Log("heejeong Glasses Success");
+
+            if (data.members[i].Hat != 0)
+            {
+                GameObject instantiatedHead = Instantiate(Bags[data.members[i].Hat - 1], transform.Find("Neck_M/Head_M"));
+                instantiatedHead.transform.position = Head.transform.position;
+                instantiatedHead.transform.rotation = Head.transform.rotation;
+                instantiatedHead.SetActive(true);
+            }
+
+            Debug.Log("heejeong Hat Success");
 
             Destroy(Back);
             Destroy(Neck);
@@ -104,9 +123,59 @@ public class AroundPeopleManager : MonoBehaviour
         }
     }
 
-    void ReceiveNearByUsers(string jsonMessage)
+    /**
+     * 클릭한 사용자 상태 조회
+     */
+    void RequestFriendStats(long memberId)
+    {
+        Debug.Log("heejeong SocialController Call: RequestFriendStats(" + memberId + ")");
+        string androidMethod = "getFriendStats";
+        _pluginInstance.Call(androidMethod, memberId);
+    }
+
+    void ReceiveFriendStats(string jsonMessage)
+    {
+        int[] scores = new int[6];
+
+        Debug.Log("heejeong [ReceiveFriendStats]" + jsonMessage);
+        MemberStats datas = JsonUtility.FromJson<MemberStats>(jsonMessage);
+
+        //for (var i = 0; i < scores.Length; i++)
+        //{
+        //    Debug.Log("heejeong 유저 스탯 점수::" + datas.stats[i].Score);
+        //    Debug.Log("stat type : " + datas.stats[i].Score.GetType().Name);
+        //    Debug.Log("heejeong 유저 스탯 레벨::" + datas.stats[i].Level);
+        //    Debug.Log("level type : " + datas.stats[i].Level.GetType().Name);
+        //}
+    }
+
+    /**
+     * 주위 사용자 조회
+     */
+    public void RequestNearByUsers()
+    {
+        Debug.Log("heejeong SocialController Call: RequestNearByUsers");
+        string androidMethod = "getNearByUsers";
+        _pluginInstance.Call(androidMethod);
+    }
+
+    public void ReceiveNearByUsers(string jsonMessage)
     {
         Debug.Log("heejeong [ReceiveNearByUsers]" + jsonMessage);
         data = JsonUtility.FromJson<AroundMembers>(jsonMessage);
+        foreach (AroundMember it in data.members)
+        {
+            Debug.Log("heejeong 근처 사용자::" + it.ToString());
+        }
+    }
+
+    /**
+     * 주위 사용자 클릭 시 사용자 인싸력 상승
+     */
+    void RequestClickOtherUser(long memberId)
+    {
+        Debug.Log("heejeong [RequestClickOtherUser]" + memberId);
+        string androidMethod = "clickOtherUser";
+        _pluginInstance.Call(androidMethod, memberId);
     }
 }
