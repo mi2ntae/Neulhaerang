b4705fdd6fc44ce44dfb4592c93f19273b234355
kosigi19 <kosigi19@naver.com>
2023-11-16 12:58:33 +0900

    Fix: PermissionsRationaleActivity Preview 삭제

--- android/app/src/main/java/com/finale/neulhaerang/PermissionsRationaleActivity.kt
+++ android/app/src/main/java/com/finale/neulhaerang/PermissionsRationaleActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 
@@ -25,7 +24,6 @@ class PermissionsRationaleActivity : ComponentActivity() {
     }
 }
 
-@Preview
 @Composable
 fun MyWebView() {
     val context = LocalContext.current
