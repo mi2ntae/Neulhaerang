f2694aa093b6577b8b9c4fc4f9bf6f8a38ed4688
jungeunevepark <evecomcom98@naver.com>
2023-11-19 09:38:22 +0900

    Fix: 편지 스케줄러 시간 수정

--- server/src/main/java/com/finale/neulhaerang/global/scheduler/MidnightScheduler.java
+++ server/src/main/java/com/finale/neulhaerang/global/scheduler/MidnightScheduler.java
@@ -69,7 +69,7 @@ public class MidnightScheduler {
 
 	@Async
 	@Transactional(readOnly = true)
-	@Scheduled(cron = "${schedules.cron.daily-routine}", zone = "Asia/Seoul")
+	@Scheduled(cron = "0 41 9 * * *", zone = "Asia/Seoul")
 	public void createLetterTrigger() {
 		log.info("---------- 자정 스케줄러 : 편지를 전송합니다 ----------");
 		List<Member> memberList = memberRepository.findAllByWithdrawalDateIsNull();
